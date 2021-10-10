39f8f03ea22edc56b21c189ef58bc18cb8a824d1
Manuel Brandstetter <accounts@manuelbrandstetter.de>
2021-10-10 13:46:18 +0200

    Add basic functionality

--- src/API/REST/routes/register.ts
+++ src/API/REST/routes/register.ts
@@ -1,12 +1,26 @@
 import express, {Router} from "express";
+import {userModel} from "../../../database/models/user";
+import bcrypt from "bcrypt";
 
 export default function register_route(): Router {
     let router = express.Router();
 
     router
         .post("/register")
-        .use(((req, res) => {
-            res.send("User has been registered");
+        .use((async (req, res) => {
+            const {firstName, lastName, email, password} = req.body;
+
+            if (await userModel.findOne({email})) {
+                return res.status(409).send("User with the given email already exists");
+            }
+
+            const encryptedPassword = await bcrypt.hash(password, 15);
+
+            const user = await userModel.create({
+                firstName, lastName, email, password: encryptedPassword
+            })
+
+            res.status(201).json(user);
         }))
 
     return router;
