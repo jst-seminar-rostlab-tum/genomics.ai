2ecd1d2100c22c7f884c29d399944cd714370e16
Matthias <ga87mit@mytum.de>
2021-10-11 23:23:41 +0200

    Added check for academic affiliation

--- src/API/REST/routes/register.ts
+++ src/API/REST/routes/register.ts
@@ -3,6 +3,7 @@ import {userModel} from "../../../database/models/user";
 import { tokenModel } from "../../../database/models/token";
 import bcrypt from "bcrypt";
 import { send_verification_mail } from "../../../util/mailer";
+import { Verifier } from 'academic-email-verifier';
 
 export default function register_route(): Router {
     let router = express.Router();
@@ -16,9 +17,13 @@ export default function register_route(): Router {
             }
 
             const encryptedPassword = await bcrypt.hash(password, 15);
-
+            const institutionName = await Verifier.getInstitutionName(email);
+            
             const user = await userModel.create({
-                firstName, lastName, email, password: encryptedPassword
+                firstName, lastName, email, 
+                password: encryptedPassword,
+                academicAffiliation: institutionName,
+                isAcademic: institutionName !== ''
             });
 
             const token = await tokenModel.create({ _userId: user._id });
--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -6,6 +6,8 @@ interface IUser extends Document {
     email: string;
     password: string;
     isVerified: boolean; // Only for e-mail, not for academic affiliation
+    isAcademic: boolean;
+    academicAffiliation: string;
 }
 
 const userSchema = new Schema<IUser>({
@@ -13,7 +15,9 @@ const userSchema = new Schema<IUser>({
     lastName: {type: String, default: ""},
     email: {type: String, unique: true, require: true},
     password: {type: String, require: true},
-    isVerified: {type: Boolean, default: false}
+    isVerified: {type: Boolean, default: false},
+    isAcademic: {type: Boolean, default: false},
+    academicAffiliation: {type: String}
 });
 
 export const userModel = model<IUser>("User", userSchema);
