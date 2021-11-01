c90dff604eeeb110fccc43a8a8e33855bbfa13cf
Lars Frölich <lars.froelich@gmail.com>
2021-11-01 16:13:14 +0100

    implemented "project" table mongoose Schema

---
+++ src/database/models/project.ts
@@ -0,0 +1,39 @@
+import {Document, model, Schema} from "mongoose";
+
+enum ProjectJobStatus {
+    "UPLOAD_PENDING",
+    "UPLOAD_COMPLETE",
+    "PROCESSING_PENDING",
+    "ABORTED",
+    "DONE"
+}
+
+export interface IProject extends Document {
+    owner: Schema.Types.ObjectId;
+
+    // file
+    uploadId: string;
+    location: string;
+    fileName: string;
+    fileSize: number; // (of bytes)
+    uploadDate: Date;
+
+    // project
+    status: string;
+}
+
+const projectSchema = new Schema<IProject>({
+    owner: {type: Schema.Types.ObjectId, require: true},
+
+    // file
+    uploadId: {type: String, require: false},
+    fileName: {type: String, require: false},
+    location: {type: String, require: false},
+    fileSize: {type: Schema.Types.Number, require: false, default: -1},
+    uploadDate: {type: Schema.Types.Date, require: true},
+
+    // project
+    status: {type: String, require: true, enum: ProjectJobStatus}
+});
+
+export const projectModel = model<IProject>("Project", projectSchema);
