a7a01ba1b4927f94e81ece1c81625c2af5f79cf2
Manuel Brandstetter <accounts@manuelbrandstetter.de>
2021-11-01 17:50:46 +0100

    Add get_profile route

---
+++ src/API/REST/routes/get_profile.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import {ExtRequest} from "../../../definitions/ext_request";
+import {IUser, userModel} from "../../../database/models/user";
+import check_auth from "../middleware/check_auth";
+
+export default function get_profile_route() {
+  let router = express.Router();
+
+  router
+    .get("/get_profile/:id", check_auth(), async (req: ExtRequest, res: any) => {
+      const userId = req.params.id;
+
+      try {
+        const user = await userModel.findOne({_id: userId});
+        return res.status(200).json(user!.toJSON());
+      } catch (e) {
+        return res.status(500);
+      }
+
+
+    });
+  return router;
+}
