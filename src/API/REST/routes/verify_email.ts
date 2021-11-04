a00cfc95acbe6e88862b29fe462ba68340063af1
Manuel Brandstetter <accounts@manuelbrandstetter.de>
2021-11-04 19:02:58 +0100

    Add html response to verify email route

--- src/API/REST/routes/verify_email.ts
+++ src/API/REST/routes/verify_email.ts
@@ -6,13 +6,15 @@ export default function verify_email_route() {
   let router = express.Router();
 
   router
-    .post("/verify_email/:token", async (req: ExtRequest, res: any) => {
+    .get("/verify_email/:token", async (req: ExtRequest, res: any) => {
       const verificationToken = req.params.token;
 
       try {
         await userModel.findOneAndUpdate({emailVerificationToken: verificationToken}, {isVerified: true});
 
-        return res.status(200).send("Your email address has been verified!");
+        return res
+          .set("Content-Type", "text/html")
+          .send(Buffer.from("<h1>Email verified</h2><p>Congratulations, you have successfully verified your email address!</p>"));
       } catch (err) {
         return res.status(500).send(err);
       }
