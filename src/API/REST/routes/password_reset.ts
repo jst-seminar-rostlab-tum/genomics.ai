206767daba599f3db71c268af5c12c325e587734
Lars Frölich <lars.froelich@gmail.com>
2021-11-20 10:59:54 +0100

    password-reset: fixed missing www., added comments/TODOs, missing await, etc

--- src/API/REST/routes/password_reset.ts
+++ src/API/REST/routes/password_reset.ts
@@ -9,19 +9,25 @@ export default function password_reset_route() : Router {
     
     router.post('/password_reset', async (req, res) => {
       const {email} = req.body;
-
-      if (!email) {
+      if (!email)
         return res.status(400).send('Missing parameters');
-      }
 
+      // check user with this email exists
       const user = await userModel.findOne({email});
+      if (!user)
+        return res.status(404).send("User not found");
 
-      if (user) {
-        const token = await passwordResetTokenModel.create({
-          _userId: user._id,
-        });
-        mailer.send(user.email, "[GeneCruncher] Please reset your password", "password_reset_request_email", {firstname: user.firstName, link: `https://genecruncher.com/password_reset/${token.token}`})
-      }
+      // TODO: check for existing tokens - don't send mail if there already is a recent token. Delete old tokens!
+
+      // create a reset-token and send email
+      const token = await passwordResetTokenModel.create({
+        _userId: user._id,
+      });
+      await mailer.send(user.email, "[GeneCruncher] Please reset your password", "password_reset_request_email", {
+        firstname: user.firstName,
+        link: `https://www.genecruncher.com/password_reset?token=${token.token}`
+      })
+
       return res.status(200).send('An email has been sent to your email address with instructions to reset your password');
     });
     
@@ -30,7 +36,7 @@ export default function password_reset_route() : Router {
       if (!password)
         return res.status(400).send("Missing parameters");
 
-      // find token corresponding to token-id
+      // find token corresponding to token-id and delete it
       const token = await passwordResetTokenModel.findOne({token: req.params.token});
       if (!token)
         return res.status(404).send("Token could not be found. It may have been expired or used already");
@@ -46,7 +52,7 @@ export default function password_reset_route() : Router {
       user.save();
 
       // send pw-update email
-      await mailer.send(user.email, "[GeneCruncher] Your password was reset", "password_reset_confirmation_email", {
+      await mailer.send(user.email, "[GeneCruncher] Your password was successfully reset", "password_reset_confirmation_email", {
         firstname: user.firstName,
         email: user.email
       });
