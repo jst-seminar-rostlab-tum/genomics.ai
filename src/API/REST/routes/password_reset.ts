afdf945ecb0ca09d3dfb96d50ccb299c1e4161f8
Lars Frölich <lars.froelich@gmail.com>
2021-11-20 10:29:43 +0100

    improved pw-reset formatting

--- src/API/REST/routes/password_reset.ts
+++ src/API/REST/routes/password_reset.ts
@@ -27,23 +27,30 @@ export default function password_reset_route() : Router {
     
     router.post('/password_reset/:token', async (req, res) => {
       const {password} = req.body;
-      if (!password) {
+      if (!password)
         return res.status(400).send("Missing parameters");
-      }
 
+      // find token corresponding to token-id
       const token = await passwordResetTokenModel.findOne({token: req.params.token});
-      if (!token) {
+      if (!token)
         return res.status(404).send("Token could not be found. It may have been expired or used already");
-      }
       token.deleteOne();
 
+      // find user indicated by token's user-field
       const user = await userModel.findById(token._userId);
-      if (!user) {
+      if (!user)
         return res.status(404).send("Invalid token: User for this token could not be found.");
-      }
+
+      // update the password
       user.password = await bcrypt.hash(password, 15);
       user.save();
-      mailer.send(user.email, "[GeneCruncher] Your password was reset", "password_reset_confirmation_email", {firstname: user.firstName, email: user.email});
+
+      // send pw-update email
+      await mailer.send(user.email, "[GeneCruncher] Your password was reset", "password_reset_confirmation_email", {
+        firstname: user.firstName,
+        email: user.email
+      });
+
       return res.status(200).send("Password has been changed");
     });
     
