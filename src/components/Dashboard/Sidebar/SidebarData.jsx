ae2e3cb24cff2cadf6c23d5c61ff5994545e0747
aminbensaad <aminbensaad@rocketmail.com>
2021-11-25 02:50:31 +0100

    removed components from Array

--- src/components/Dashboard/Sidebar/SidebarData.jsx
+++ src/components/Dashboard/Sidebar/SidebarData.jsx
@@ -1,28 +1,19 @@
-import React from 'react';
-
-import * as IoIcons from 'react-icons/io';
-import * as CGIcons from 'react-icons/cg';
-
 export const SidebarData = [
   {
     id: 0,
     name: 'Project 1',
     path: '/project1',
     // needed in case sub items exist
-    iconClosed: <IoIcons.IoIosArrowForward />,
-    iconOpened: <IoIcons.IoIosArrowDown />,
     subNav: [
       {
         id: 0,
         name: 'celltype annot. 1',
         path: '/overview/ca1',
-        icon: <CGIcons.CgFileDocument />,
       },
       {
         id: 1,
         name: 'celltype annot. 2',
         path: '/overview/ca2',
-        icon: <CGIcons.CgFileDocument />,
       },
     ],
   },
@@ -31,18 +22,14 @@ export const SidebarData = [
     id: 1,
     name: 'Project 2',
     path: '/project2',
-    iconClosed: <IoIcons.IoIosArrowForward />,
-    iconOpened: <IoIcons.IoIosArrowDown />,
     subNav: [
       {
         name: 'celltype annot. 1',
         path: '/overview/ca1',
-        icon: <CGIcons.CgFileDocument />,
       },
       {
         name: 'celltype annot. 2',
         path: '/overview/ca2',
-        icon: <CGIcons.CgFileDocument />,
       },
     ],
   },
