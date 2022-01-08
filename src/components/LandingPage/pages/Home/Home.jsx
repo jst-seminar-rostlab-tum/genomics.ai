b31cd107d57974a5f475ee716144491b8e2cd4ad
aminbensaad <aminbensaad@rocketmail.com>
2022-01-08 02:37:12 +0100

    started implementing call 2 action at top of page

--- src/components/LandingPage/pages/Home/Home.jsx
+++ src/components/LandingPage/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Typography, Box, Divider, Stack
+  Typography, Box, Divider, Stack,
 } from '@mui/material';
 import NavBar from '../../../NavBar/NavBar';
 import styles from './home.module.css';
@@ -18,9 +18,30 @@ function Home() {
   return (
     <div className={styles.container}>
       <NavBar />
-      <img className={styles.backgroundImage} src={dnaImage} src={dnaImage} alt="picture of DNA" />
       {/* If genomics visualized stays in the middle, then it needs to be properly aligned.
       Right now it is using padding as the way to align the items */}
+
+      <div className={styles.call2Action}>
+        <Typography
+          sx={{ fontSize: '55px', fontWeight: 'bold', color: 'white' }}
+        >
+          AI-driven Cell Type Annotation
+        </Typography>
+        <Typography
+          sx={{ fontSize: '55px', fontWeight: 'regular', color: 'white' }}
+        >
+          No Code. Just Results.
+        </Typography>
+        <Typography
+          sx={{ fontSize: '25px', fontWeight: 'regular', color: 'white', paddingTop: '40px'}}
+        >
+          Genomics.ai helps you visualize all of your single-cell sequencing data
+          in a fast and easy wayusing neural networks.
+        </Typography>
+      </div>
+
+      <img className={styles.backgroundImage} src={dnaImage} alt="picture of DNA" />
+
       <Divider variant="middle" textAlign="center" sx={{ padding: '30px', paddingBottom: '50px', paddingTop: '100px' }}>
         <Typography sx={{ fontSize: '35px', fontWeight: 'bold' }}>Our Partners</Typography>
       </Divider>
@@ -45,8 +66,10 @@ function Home() {
       <div className={styles.infoContainer}>
         <img className={styles.illustration} src={graphic1} alt="science-guy" />
         <div className={styles.explanation}>
-          <Typography className={styles.illustrationTitle} sx={{ fontSize: '28px', fontWeight: 'bold' }}>Genomics.ai</Typography>
-          <Typography sx={{ fontSize: '25px' }}> We help you visualize all
+          <Typography className={styles.illustrationTitle} sx={{ fontSize: '30px', fontWeight: 'bold' }}>Genomics.ai</Typography>
+          <Typography sx={{ fontSize: '25px' }}>
+            {' '}
+            We help you visualize all
             of your single-cell sequencing data in a fast and easy way with the help of neural networks.
           </Typography>
         </div>
@@ -58,7 +81,7 @@ function Home() {
 
       <div className={styles.infoContainer}>
         <div className={styles.explanation}>
-          <Typography className={styles.illustrationTitle} sx={{ fontSize: '28px', fontWeight: 'bold' }}>Upload</Typography>
+          <Typography className={styles.illustrationTitle} sx={{ fontSize: '30px', fontWeight: 'bold' }}>Upload</Typography>
           <Typography sx={{ fontSize: '25px' }}>Upload your files containing the single-cell sequencing data.</Typography>
         </div>
         <img className={styles.illustration} src={graphic2} alt="upload" />
@@ -67,7 +90,7 @@ function Home() {
       <div className={styles.infoContainer}>
         <img className={styles.illustration} src={graphic3} alt="processing" />
         <div className={styles.explanation}>
-          <Typography className={styles.illustrationTitle} sx={{ fontSize: '28px', fontWeight: 'bold' }}>Processing</Typography>
+          <Typography className={styles.illustrationTitle} sx={{ fontSize: '30px', fontWeight: 'bold' }}>Processing</Typography>
           <Typography sx={{ fontSize: '25px' }}>
             Your input data is processed by our machine learning model
             which performs the cell-type classification according to your specification.
@@ -77,9 +100,11 @@ function Home() {
 
       <div className={styles.infoContainer}>
         <div className={styles.explanation}>
-          <Typography className={styles.illustrationTitle} sx={{ fontSize: '28px', fontWeight: 'bold' }}>Check Results</Typography>
-          <Typography sx={{ fontSize: '25px' }}>After the algorithm has processed the data you can
-          specify the project you want your cell-type data to be associated with and view the results.</Typography>
+          <Typography className={styles.illustrationTitle} sx={{ fontSize: '30px', fontWeight: 'bold' }}>Check Results</Typography>
+          <Typography sx={{ fontSize: '25px' }}>
+            After the algorithm has processed the data you can
+            specify the project you want your cell-type data to be associated with and view the results.
+          </Typography>
         </div>
         <img className={styles.illustration} src={graphic4} alt="results" />
       </div>
