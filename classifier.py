c5f16183ee9d1edb473365ad1f6f1d72706b8e43
YRC99 <49617168+YRC99@users.noreply.github.com>
2022-01-09 10:37:13 +0100

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -72,7 +72,7 @@ def classify():
         os.remove(result)
 
     db.projects.update_one({'uploadId':uploadId }, {
-                            "$set": {"status": "Prediction Done", "resultSize": uploadSize}})
+                            "$set": {"status": "DONE", "resultSize": uploadSize}})
     print("Classification has been computed")
     return "Classification has been computed", 200
     
