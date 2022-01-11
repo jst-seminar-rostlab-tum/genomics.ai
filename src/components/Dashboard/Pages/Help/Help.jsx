a091c2f9afae11cd12c714dd39f0a4c53a651583
aminbensaad <aminbensaad@rocketmail.com>
2022-01-11 15:59:31 +0100

    added Help mock to dashboard content - more in progress

--- src/components/Dashboard/Pages/Help/Help.jsx
+++ src/components/Dashboard/Pages/Help/Help.jsx
@@ -1,12 +1,112 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import {
+  Stack, ThemeProvider, createTheme, Typography, TextField, Button,
+} from '@mui/material';
+import styles from './help.module.css';
 
-const Help = () => (
-  <div style={{
-    display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh',
-  }}
-  >
-    <h1>Help</h1>
-  </div>
-);
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0075FF',
+    },
+    secondary: {
+      main: '#FFFFFF',
+    },
+  },
+});
 
-export default Help;
+function Documentation({ sidebarShown }) {
+  const paddingL = useCallback(() => (sidebarShown ? '130px' : '380px'), [sidebarShown]);
+
+  const [contactDetails, setContactDetails] = useState({
+    message: '',
+  });
+
+  const handleTextChange = useCallback((e) => {
+    setContactDetails((prevState) => ({ ...prevState, [e.target.id]: e.target.value }));
+  }, [setContactDetails]);
+
+  const formSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      message: contactDetails.message,
+    };
+    console.log(data);
+
+    try {
+      // send POST to the server
+      // await axios.post("BACKEND_URL", data);
+      setContactDetails({
+        message: '...sending',
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+
+    <ThemeProvider theme={theme}>
+      <Stack
+        direction="column"
+        sx={{
+          paddingTop: '100px',
+          paddingLeft: paddingL,
+          paddingRight: '30px',
+          display: 'flex',
+          width: '100%',
+          justifyContent: 'center',
+        }}
+        spacing={1}
+      >
+        <div className={styles.title}>
+          <h1>Help</h1>
+        </div>
+
+        <Stack direction="column" spacing={4}>
+          <Stack spacing={0.5}>
+            <Typography sx={{ fontSize: '20px' }}>
+              Please describe to us the problem you are experiencing.
+            </Typography>
+            <Typography sx={{ fontSize: '20px' }}>
+              We will get back to you within the next 48 hours.
+            </Typography>
+          </Stack>
+
+          <TextField
+            id="message"
+            label="Please describe your problem"
+            placeholder="Enter your message"
+            variant="filled"
+            multiline
+            rows={8}
+            rowsMax={20}
+            value={contactDetails.message}
+            onChange={handleTextChange}
+            required
+            type="text"
+            sx={{ maxWidth: '1000px' }}
+          />
+
+          <Button
+            variant="contained"
+            style={{
+              height: '60px', width: '180px', borderRadius: '10px', fontWeight: 'bold',
+            }}
+            size="large"
+            onClick={formSubmit}
+            color="primary"
+          >
+            Send
+          </Button>
+        </Stack>
+
+      </Stack>
+
+    </ThemeProvider>
+
+  );
+}
+
+export default Documentation;
