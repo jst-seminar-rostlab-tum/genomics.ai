961869ecabc104d3a4e9306f2e0acd053b290d68
Pellumb Baboci <pellumbbaboci@gmail.com>
2022-04-15 16:29:55 +0200

    added auth_check middleware to the newly added endpoints

--- Backend/src/API/REST/routes/institution/institutionRouter.ts
+++ Backend/src/API/REST/routes/institution/institutionRouter.ts
@@ -1,34 +1,33 @@
 import express, {Router} from "express";
 import {IInstitution, institutionModel} from "../../../../database/models/institution";
 import { userModel } from "../../../../database/models/user";
+import check_auth from "../../middleware/check_auth";
 
 export default function institution_route() : Router {
     let router = express.Router();
 
     router
-        .post("/institutions", async (req: any, res) => {
-            
-            const {name, country, profilePictureURL, backgroundPictureURL, adminId, invitedMemberIds, projects} = req.body;
+        .post("/institutions", check_auth(), async (req: any, res) => {
+        
+            const {name, country, profilePictureURL, backgroundPictureURL} = req.body;
+            const admin_user_id = req.user_id;
 
-            if (!(name && country && adminId))
+            if (!(name && country && admin_user_id))
                 return res.status(400).send("Missing parameters");
 
             if (await institutionModel.findOne({name}))
                 return res.status(409).send("Institution with the given name already exists!");
 
-            if (! await userModel.findOne({_id: adminId}))
+            if (! await userModel.findOne({_id: admin_user_id}))
                 return res.status(404).send("Admin that you are trying to assign does not exists!");   
 
-            let institution : (IInstitution | undefined) = undefined;
             try{
-                institution = await institutionModel.create({
+                const institution = await institutionModel.create({
                     name: name,
                     country: country,
                     profilePictureURL,
                     backgroundPictureURL,
-                    invitedMemberIds,
-                    projects,
-                    adminIds: [adminId]
+                    adminIds: [admin_user_id]
                 });
 
                 return res.status(201).json(institution);
--- Backend/src/API/REST/routes/project/projectRouter.ts
+++ Backend/src/API/REST/routes/project/projectRouter.ts
@@ -2,36 +2,32 @@ import express, {Router} from "express";
 import { IProject, projectModel } from "../../../../database/models/project";
 import { userModel } from "../../../../database/models/user";
 import { visibilityStatus } from "../../../../database/models/project";
+import check_auth from "../../middleware/check_auth";
 
 export default function project_route() : Router {
     let router = express.Router();
 
     router
-        .post("/projects", async (req: any, res) => {
+        .post("/projects", check_auth(), async (req: any, res) => {
             
-            const {title, description, adminId, invitedMemberIds, memberIds, visibility, projectJobs} = req.body;
+            const {title, description, visibility} = req.body;
+            const admin_user_id = req.user_id;
 
-            if (!(title && description && visibility && adminId))
+            if (!(title && description && visibility && admin_user_id))
                 return res.status(400).send("Missing parameters");
 
             if (await projectModel.findOne({title}))
                 return res.status(409).send("Project with the given name already exists!");
 
-            if (! await userModel.findOne({_id: adminId}))
+            if (! await userModel.findOne({_id: admin_user_id}))
                 return res.status(404).send("Admin that you are trying to assign does not exists!");   
 
-            let project : (IProject | undefined) = undefined;
-
-            // TODO: think again the way of getting ids response and how to store the ids 
             try{
-                project = await projectModel.create({
+                const project = await projectModel.create({
                     title,
                     description,
-                    invitedMemberIds,
-                    memberIds,
                     visibility,
-                    projectJobs,
-                    adminIds: [adminId]
+                    adminIds: [admin_user_id]
                 });
 
                 return res.status(201).json(project);
