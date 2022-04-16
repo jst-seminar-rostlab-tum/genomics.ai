231d317d9e357f7fc7ecf1508c8ec01f8eb3cb1c
Paul Schwind <pal03377@gmail.com>
2022-04-16 22:42:47 +0200

    move ProfileImage to own component & use in header & on settings page

--- Frontend/src/components/Header/index.jsx
+++ Frontend/src/components/Header/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './header.module.css';
+import ProfileImage from 'components/ProfileImage';
 
 function Header({
   title,
+  replaceRight,
 }) {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>{title}</h1>
+      <div className={styles.right}>
+        {replaceRight == null
+          ? <ProfileImage sizePixels={44} /> : replaceRight}
+      </div>
     </header>
   );
 }
