89b81a3503e53dcd9b50979e5788b5b92463af0e
Elif Çalışkan <elif.caliskan@tum.de>
2022-04-18 00:19:47 +0200

    Create Tag component

--- Frontend/src/components/Filter/index.jsx
+++ Frontend/src/components/Filter/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormGroup from '@mui/material/FormGroup';
 import Box from '@mui/material/Box';
 import MenuItem from '@mui/material/MenuItem';
@@ -8,27 +8,44 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import styles from './filter.module.css';
 import { Divider, Stack, TextField } from '@mui/material';
+import Tag from '../Tag';
 
 export default function Filter() {
-  const [reference, setReference] = React.useState('');
+  const [reference, setReference] = useState('');
+  const [category, setCategory] = useState('');
+  const [selectedIndexes, setSelectedIndexes] = useState([]);
+  const references = ['Human - PBMC', 'Human - PBMC2', 'Human - PBMC3', 'Human - PBMC4', 'Human - PBMC5'];
+  const categories = ['lung', 'lung2', 'lung3', 'lung4', 'lung5'];
 
   const handleReferenceChange = (event) => {
     setReference(event.target.value);
   };
 
-  const [category, setCategory] = React.useState('');
-
   const handleCategoryChange = (event) => {
     setCategory(event.target.value);
   };
 
+  const handleIndexesAppend = (newValue) => {
+    setSelectedIndexes((array) => [...array, newValue]);
+  };
+
+  const addCategory = () => {
+    if (!selectedIndexes.includes(category) && category) {
+      handleIndexesAppend(category);
+    }
+  };
+
+  const removeTag = (selectedIndex) => {
+    setSelectedIndexes(selectedIndexes.filter((item) => item !== selectedIndex));
+  };
+
   return (
-    <Box sx={{ width: 420 }}>
+    <Box sx={{ maxWidth: 420 }}>
       <FormGroup>
         <Stack
           className="flexContainer"
           direction="row"
-          sx={{ margin: 4 }}
+          sx={{ margin: 2 }}
 
         >
           <FormControlLabel control={<Checkbox />} label={<Typography sx={{ fontWeight: 'bold' }}>Referenced By Atlas</Typography>} />
@@ -40,16 +57,16 @@ export default function Filter() {
             select
             variant="standard"
           >
-            <MenuItem value="Human - PBMC">Human - PBMC</MenuItem>
-            <MenuItem value="Human - PBMC2">Human - PBMC2</MenuItem>
-            <MenuItem value="Human - PBMC3">Human - PBMC3</MenuItem>
+            {references.map((referenceItem, index) => (
+              <MenuItem value={index}>{referenceItem}</MenuItem>
+            ))}
           </TextField>
         </Stack>
-        <Divider />
+        <Divider sx={{ marginX: 2 }} />
         <Stack
           className="flexContainer"
           direction="row"
-          sx={{ margin: 4 }}
+          sx={{ margin: 2 }}
         >
           <FormControlLabel control={<Checkbox />} label={<Typography sx={{ fontWeight: 'bold' }}>Category</Typography>} />
           <TextField
@@ -60,11 +77,20 @@ export default function Filter() {
             select
             variant="standard"
           >
-            <MenuItem value="lung">lung</MenuItem>
-            <MenuItem value="lung2">lung2</MenuItem>
-            <MenuItem value="lung3">lung3</MenuItem>
+            {categories.map((categoryItem, index) => (
+              <MenuItem value={index}>{categoryItem}</MenuItem>
+            ))}
           </TextField>
-          <Button variant="contained">Add</Button>
+          <Button variant="contained" sx={{ marginX: 2 }} onClick={addCategory}>Add</Button>
+        </Stack>
+        <Stack
+          className="flexContainer"
+          direction="row"
+          sx={{ marginX: 2, marginBottom: 4 }}
+        >
+          {selectedIndexes.map((selectedIndex) => (
+            <Tag text={categories[selectedIndex]} handleClick={() => removeTag(selectedIndex)} />
+          ))}
         </Stack>
       </FormGroup>
 
