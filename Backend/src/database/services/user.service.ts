6f9c4d9c2e659a4f9a7d1ca78ac9612b5a0d5638
Atamert Rahma <atamertiel@gmail.com>
2022-04-18 17:54:57 +0200

    Removed unnecessary methods

--- Backend/src/database/services/user.service.ts
+++ Backend/src/database/services/user.service.ts
@@ -5,7 +5,7 @@ import {ObjectId} from "mongoose"
 /**
  *  @class UserService
  *
- *  Provides useful methods to access the database,
+ *  Provides useful methods to access the database and modify users,
  *  which can be used by the route-controllers.
  */
 export default class UserService {
@@ -15,10 +15,8 @@ export default class UserService {
      *  @returns  allUsers
      */
     static async getAllUsers():
-      Promise<(IUser & {_id: any })[]> {
-        const allUsers = await userModel.find().exec();
-        console.log("userService.getAllUsers : " + allUsers); // TODO
-        return allUsers;
+      Promise<( IUser & { _id: ObjectId } )[]> {
+        return await userModel.find().exec();
     }
 
     /**
@@ -28,8 +26,8 @@ export default class UserService {
      *  @param   includePassword - if true, the returned user includes the password field
      *  @returns user - matched user to user_id or null
      */
-    static async getUserById(user_id: (ObjectId | string), includePassword = false):
-      Promise<( IUser & { _id: any } | null )> {
+    static async getUserById(user_id: (ObjectId | string), includePassword: boolean = false):
+      Promise<( IUser & { _id: ObjectId } | null )> {
         return includePassword ?
           await userModel.findById(user_id).select('+password').exec() :
           await userModel.findById(user_id).exec();
@@ -42,8 +40,8 @@ export default class UserService {
      *  @param   includePassword - if true, the returned user includes the password field
      *  @returns user - matched user to email or null
      */
-    static async getUserByEmail(email: string, includePassword = false):
-      Promise<( IUser & { _id: any } | null )> {
+    static async getUserByEmail(email: string, includePassword: boolean = false):
+      Promise<( IUser & { _id: ObjectId } | null )> {
         return includePassword ?
           await userModel.findOne({email}).select('+password').exec() :
           await userModel.findOne({email}).exec();
@@ -55,7 +53,7 @@ export default class UserService {
      *  @returns users - unauthorized users or null
      */
     static async getUnauthUsers():
-      Promise<( IUser & { _id: any } | null )[]> {
+      Promise<( IUser & { _id: ObjectId } | null )[]> {
         return await userModel.find({isAuthorized: false}).exec();
     }
 
@@ -71,30 +69,14 @@ export default class UserService {
         return userAdded;
     }
 
-    static async addUsers(users: [AddUserDTO]) {
-        // TODO
-        let usersStr = "";
-        users.forEach(user => usersStr += JSON.stringify(user, null, 2));
-        console.log("userService.addUsers : adding users " + usersStr);
-
-        let usersAdded : ([IUser] | undefined) = undefined;
-
-        // TODO DEBUG
-        //let usersAddedStr = "";
-        //usersAdded.forEach(userAdded => usersAddedStr += userAdded.toObject());
-        //console.log("userService.addUser : " + userAdded.toObject()); // TODO
-        //return userAddedStr.toObject();
-    } // TODO
-
-
     /**
-     *  TODO test
+     *  Updates the given user corresponding to the user_id with the
+     *  update_object.
+     *
+     *  @param user_id
+     *  @param update_object - includes fields to be updated
      */
     static async updateUser(user_id: (ObjectId | string), update_object: UpdateUserDTO) {
         await userModel.updateOne({_id: user_id}, update_object);
     }
-
-    static async deleteUser() {} // TODO
-
-    static async deleteUsers() {} // TODO
 }
