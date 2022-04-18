0fce4cff719102422469cf7ea8be5dfa5c8b2c4c
rei <reiballa@gmail.com>
2022-04-18 17:47:18 +0200

    adding our colors

---
+++ Frontend/src/shared/theme/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles" 
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "",
+      
+    },
+    secondary: {
+      main: ""
+    },
+    typography: {
+      fontFamily: ""
+    },
+
+  }
+})
