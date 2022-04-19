b948b33996e026660f61703d12a9fbc761dacd99
Atamert Rahma <atamertiel@gmail.com>
2022-04-20 00:49:51 +0200

    Routes updated
    Models removed from routes, they use services instead

--- Backend/src/API/REST/routes/project/projectRouter.ts
+++ Backend/src/API/REST/routes/project/projectRouter.ts
@@ -1,6 +1,7 @@
 import express, {Router} from "express";
-import { projectModel } from "../../../../database/models/project";
-import { userModel } from "../../../../database/models/user";
+import ProjectService from "../../../../database/services/project.service";
+import { AddProjectDTO } from "../../../../database/dtos/project.dto";
+import UserService from "../../../../database/services/user.service";
 import { visibilityStatus } from "../../../../database/models/project";
 import check_auth from "../../middleware/check_auth";
 
@@ -9,30 +10,33 @@ const create_project = () : Router => {
 
     router
         .post("/projects", check_auth(), async (req: any, res) => {
-            
+
             const {title, description, visibility} = req.body;
             const admin_user_id = req.user_id;
 
             if (!(title && description && visibility && admin_user_id))
                 return res.status(400).send("Missing parameters");
-            
+
             if(!(Object.values(visibilityStatus).includes(visibility)) )
                 return res.status(400).send("Visibility parameter is wrong format. You should type one of the followings: PRIVATE, PUBLIC, BY_INSTITUTION");
 
             // TODO: make this check later
-            // if (await projectModel.findOne({title}))
+            // const project = await ProjectService.getProjectByTitle(title);
+            // if (project)
             //     return res.status(409).send("Project with the given name already exists!");
 
-            if (! await userModel.findOne({_id: admin_user_id}))
-                return res.status(404).send("Admin that you are trying to assign does not exists!");   
+            const admin = await UserService.getUserById(admin_user_id);
+            if (!admin)
+                return res.status(404).send("Admin that you are trying to assign does not exists!");
 
             try{
-                const project = await projectModel.create({
+                const projectToAdd: AddProjectDTO = {
                     title,
                     description,
                     visibility,
                     adminIds: [admin_user_id]
-                });
+                };
+                const project = await ProjectService.addProject(projectToAdd);
 
                 return res.status(201).json(project);
             }catch(err){
