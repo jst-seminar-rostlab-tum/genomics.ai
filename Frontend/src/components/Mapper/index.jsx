796cd7f17e12dfd9d8a23799393fb61660831384
Elif Çalışkan <elif.caliskan@tum.de>
2022-04-20 16:15:19 +0200

    Fix positioning and remove input

--- Frontend/src/components/Mapper/index.jsx
+++ Frontend/src/components/Mapper/index.jsx
@@ -1,36 +1,43 @@
 import React, { useState } from 'react';
 import {
-  Typography, Box, Button, Input, IconButton, Divider, Stack, Fab,
+  Typography, Box, Button, IconButton, Divider, Stack, Fab,
 } from '@mui/material';
 import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
 import NoteAltIcon from '@mui/icons-material/NoteAlt';
 import CancelIcon from '@mui/icons-material/Cancel';
 import styles from './mapper.module.css';
 
-function Mapper() {
-  const [files, setFiles] = useState({
-    atlas: null,
-    model: null,
-  });
+function Mapper({
+  initialAtlas, initialModel, editAtlasClicked, editModelClicked,
+}) {
+  const [atlas, setAtlas] = useState(initialAtlas);
+  const [model, setModel] = useState(initialModel);
   const [open, setOpen] = useState(false);
+
+  const handleEditAtlas = () => {
+    editAtlasClicked();
+    setAtlas(null);
+  };
+
+  const handleEditModel = () => {
+    editModelClicked();
+    setModel(null);
+  };
 
   return (
     <Box className={styles.container}>
-      <Box className={styles.borderContainer} sx={{ boxShadow: 5, visibility: open ? 'visible' : 'hidden' }}>
+      <Box className={styles.borderContainer} sx={{ visibility: open ? 'visible' : 'hidden' }}>
         <Typography className={styles.title}>Mapper</Typography>
         <Divider className={styles.divider} />
         <Typography>Selected Atlas</Typography>
         <Stack
           direction="row"
         >
-          <Typography className={styles.filename}>{files.atlas ? files.atlas.name : 'None'}</Typography>
-          <label htmlFor="select-atlas">
-            <Input id="select-atlas" type="file" className={styles.input} onChange={(e) => setFiles({ ...files, atlas: e.target.files[0] })} />
-            <IconButton className={styles.iconButton} variant="contained" component="span">
-              <NoteAltIcon className={styles.icon} />
-            </IconButton>
-          </label>
-          <IconButton className={styles.iconButton} variant="contained" component="span" onClick={() => setFiles({ ...files, atlas: null })}>
+          <Typography className={styles.filename}>{atlas || 'None'}</Typography>
+          <IconButton className={styles.iconButton} variant="contained" component="span" onClick={handleEditAtlas}>
+            <NoteAltIcon className={styles.icon} />
+          </IconButton>
+          <IconButton className={styles.iconButton} variant="contained" component="span" onClick={() => setAtlas(null)}>
             <CancelIcon className={styles.icon} />
           </IconButton>
         </Stack>
@@ -39,14 +46,11 @@ function Mapper() {
         <Stack
           direction="row"
         >
-          <Typography className={styles.filename}>{files.model ? files.model.name : 'None'}</Typography>
-          <label htmlFor="select-model">
-            <Input id="select-model" type="file" className={styles.input} onChange={(e) => setFiles({ ...files, model: e.target.files[0] })} />
-            <IconButton className={styles.iconButton} variant="contained" component="span">
-              <NoteAltIcon className={styles.icon} />
-            </IconButton>
-          </label>
-          <IconButton className={styles.iconButton} variant="contained" component="span" onClick={() => setFiles({ ...files, model: null })}>
+          <Typography className={styles.filename}>{model || 'None'}</Typography>
+          <IconButton className={styles.iconButton} variant="contained" component="span" onClick={handleEditModel}>
+            <NoteAltIcon className={styles.icon} />
+          </IconButton>
+          <IconButton className={styles.iconButton} variant="contained" component="span" onClick={() => setModel(null)}>
             <CancelIcon className={styles.icon} />
           </IconButton>
         </Stack>
