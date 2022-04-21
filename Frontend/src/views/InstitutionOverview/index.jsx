d44fb42cefc61e47590f5b1b418df5607c1ad3cb
Paul Schwind <paul@pschwind.de>
2022-04-21 10:00:58 +0200

    institution creation dialog

--- Frontend/src/views/InstitutionOverview/index.jsx
+++ Frontend/src/views/InstitutionOverview/index.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
 import HeaderView from 'components/general/HeaderView';
 import InstitutionCard from 'components/institutionOverview/InstitutionCard';
 import styles from './institutionOverview.module.css';
 import queryMyInstitutions from 'shared/services/mock/institutions';
+import InstitutionCreationDialog from 'components/institutionOverview/InstitutionCreationDialog';
 
 function InstitutionOverview({ sidebarShown }) {
   const [institutions, setInstitutions] = useState([]);
@@ -16,24 +18,36 @@ function InstitutionOverview({ sidebarShown }) {
     setInstitutions(institutions.filter((i) => i.id !== institution.id));
   }
 
+  const [createOpen, setCreateOpen] = useState(false);
+
   return (
-    <HeaderView
-      sidebarShown={sidebarShown}
-      title="My Institutions"
-    >
-      <div className={styles.content}>
-        {institutions.length === 0 ? 'No institutions.' : ''}
-        {institutions.map((institution) => (
-          <div key={institution.id}>
-            <InstitutionCard
-              institution={institution}
-              onLeft={(inst) => onLeft(inst)}
-            />
-            <div className={styles.cardSpacing} />
-          </div>
-        ))}
-      </div>
-    </HeaderView>
+    <>
+      <HeaderView
+        sidebarShown={sidebarShown}
+        title="My Institutions"
+        replaceHeaderRight={(
+          <Button onClick={() => setCreateOpen(true)}>Create</Button>
+        )}
+      >
+        <div className={styles.content}>
+          {institutions.length === 0 ? 'No institutions.' : ''}
+          {institutions.map((institution) => (
+            <div key={institution.id}>
+              <InstitutionCard
+                institution={institution}
+                onLeft={(inst) => onLeft(inst)}
+              />
+              <div className={styles.cardSpacing} />
+            </div>
+          ))}
+        </div>
+      </HeaderView>
+      <InstitutionCreationDialog
+        open={createOpen}
+        handleClose={() => setCreateOpen(false)}
+        onCreated={(newInstitution) => setInstitutions([...institutions, newInstitution])}
+      />
+    </>
   );
 }
 
