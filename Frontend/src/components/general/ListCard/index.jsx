1a74aef93c39f6e59c37e1bee3d77413835f2f2c
Paul Schwind <paul@pschwind.de>
2022-04-21 10:44:36 +0200

    default avatar for listCard

--- Frontend/src/components/general/ListCard/index.jsx
+++ Frontend/src/components/general/ListCard/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Card from '@mui/material/Card';
+import Avatar from '@mui/material/Avatar';
 import styles from './listCard.module.css';
+import stringColor from 'shared/utils/stringColor';
 
 function InstitutionCard({
   imageURL, title, description, nextToTitle, trailing,
@@ -9,9 +11,16 @@ function InstitutionCard({
     <Card sx={{ border: '1px solid #c8c8c8', boxShadow: '0 0 4px rgba(111, 111, 111, 0.25)' }} elevation={0}>
       <div className={styles.cardContent}>
         <div className={styles.start}>
-          {imageURL && (
-            <img className={styles.cardImage} src={imageURL} alt="" />
-          )}
+          <div className={styles.cardImageWrapper}>
+            {imageURL ? (
+              <img className={styles.cardImage} src={imageURL} alt="" />
+            ) : (
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              <Avatar sx={{ backgroundColor: stringColor(title || ''), width: 42, height: 42 }}>
+                {(title || '?')[0]}
+              </Avatar>
+            )}
+          </div>
           <div className={styles.text}>
             <div className={styles.titleRow}>
               <h3 className={styles.title}>{title}</h3>
