e6dad07906b3c35709f7ef65881f95e4266ea5e4
rei <reiballa@gmail.com>
2022-04-23 01:16:23 +0200

    modal first commit

---
+++ Frontend/src/components/Modal/index.jsx
@@ -0,0 +1,42 @@
+import { Button, Typography } from "@mui/material"
+import { Box } from "@mui/system"
+import ReactDOM from "react-dom"
+import { colors } from "shared/theme/colors"
+
+const Modal = ({ 
+  message, isOpen, onClose, children
+}) => {
+  if(!isOpen) return null
+  return ReactDOM.createPortal(
+    <Box sx={{ overflow: "hidden" }}>
+      <Box sx={{ 
+        position: "absolute", top: "0", left: "0",
+        width: "100vw", height: "100vw",
+        backgroundColor: "rgba(0, 0, 0, 0.6)",
+        zIndex: "20",
+        overflow: "hidden"
+      }}/>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          backgroundColor: "white",
+          p: "2em",
+          borderRadius: "20px",
+          boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2), 0px 0px 1px rgba(0, 0, 0, 0.3)",
+          zIndex: "100"
+        }}
+        >
+        <Typography fontSize="2em" fontWeight="bold">{message}</Typography>
+        <Box sx={{ height: "1px", width: "100%", backgroundColor: colors.neutral[400], margin: "0.2em 0 1em 0" }}/>
+        {children}
+        {/* TODO Replace with new buttons */}
+        <Button onClick={onClose}>Close</Button>
+      </Box>
+    </Box>
+  , document.body)
+}
+
+export default Modal
