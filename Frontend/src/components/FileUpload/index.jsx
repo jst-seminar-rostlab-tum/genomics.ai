daeab3a65f767c5d14979eb4b12c032581da25a8
rei <reiballa@gmail.com>
2022-04-23 16:58:07 +0200

    fixed alignment and added suggestions

--- Frontend/src/components/FileUpload/index.jsx
+++ Frontend/src/components/FileUpload/index.jsx
@@ -6,18 +6,38 @@ import { colors } from "shared/theme/colors"
 
 const BeforeDrag = () => {
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "1em", color: colors.primary[800] }}>
+    <Box sx={{ 
+      display: "flex", 
+      flexDirection: "column", 
+      alignItems: "center", 
+      gap: "1em", 
+      color: colors.primary[800],
+      position: "absolute",
+      left: "50%",
+      top: "50%",
+      transform: "translate(-50%, -50%)"
+    }}>
       <CloudUpload style={{ fontSize: 80 }}/>
-      <Typography fontWeight="bold" fontSize="1.2em">Drag and drop or browse files</Typography>
+      <Typography fontWeight="bold" fontSize="1.2em" sx={{ textAlign: "center" }}>Drag and drop or browse files</Typography>
     </Box>
   )
 }
 
 const DuringDrag = () => {
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "1em", color: colors.primary[500] }}>
+    <Box sx={{ 
+      display: "flex", 
+      flexDirection: "column", 
+      alignItems: "center", 
+      gap: "1em", 
+      color: colors.primary[500], 
+      position: "absolute",
+      left: "50%",
+      top: "50%",
+      transform: "translate(-50%, -50%)"
+    }}>
       <CloudDone style={{ fontSize: 80 }}/>
-      <Typography fontWeight="bold" fontSize="1.2em">Drop files here</Typography>
+      <Typography fontWeight="bold" fontSize="1.2em" sx={{ textAlign: "center" }}>Drop files here</Typography>
     </Box>
   )
 }
@@ -45,7 +65,8 @@ const FileUpload = ({ width = "100%", height = "100%", handleFileUpload }) => {
         boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.25), 0px 0px 1px rgba(0, 0, 0, 0.32)",
         p: "3em",
         borderRadius: "20px",
-        cursor: "pointer"
+        cursor: "pointer",
+        position: "relative"
       }}
     >
      <input {...getInputProps()}/>
