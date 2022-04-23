5c5a0f6862f9cc3db62d88d6482a15101bde1319
Cem Nasit Sarica <cemnasitsarica@gmail.com>
2022-04-23 19:22:37 +0300

    Dialog to confirm institution change

--- Frontend/src/components/teams/detail/TeamHeaderOptions/index.jsx
+++ Frontend/src/components/teams/detail/TeamHeaderOptions/index.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  Chip, Stack, TextField, MenuItem,
+  Chip, Stack, TextField, MenuItem, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle
 } from '@mui/material';
 
 function TeamHeaderOptions({
   team, isAdmin, institution, availableInstitutions, setInstitution,
 }) {
-  const handleInstitutionChange = (event) => {
-    setInstitution(event.target.value);
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [targetInstitution, setTargetInstitution] = useState({});
+
+  const handleCloseDialog = () => setDialogOpen(false);
+
+  const handleOpenDialog = (event) => {
+    setDialogOpen(true);
+    setTargetInstitution(event.target.value);
+  };
+
+  const handleInstitutionChange = () => {
+    setInstitution(targetInstitution);
+    handleCloseDialog();
   };
 
   return (
@@ -27,7 +38,7 @@ function TeamHeaderOptions({
             select
             label="Institution"
             value={institution}
-            onChange={handleInstitutionChange}
+            onChange={handleOpenDialog}
             variant="standard"
           >
             {availableInstitutions.map((institutionOption) => (
@@ -40,6 +51,28 @@ function TeamHeaderOptions({
             ))}
           </TextField>
         )}
+
+      <Dialog
+        open={dialogOpen}
+        onClose={handleCloseDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          Change Institution
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {`Do you really want to move the team ${team.name} to the institution ${targetInstitution.name}?`}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseDialog}>Cancel</Button>
+          <Button onClick={handleInstitutionChange} color="critical" autoFocus>
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
 
     </Stack>
   );
