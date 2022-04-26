68d5c6984d2bb5baf4096d91fbbd2e7771de4cbd
rei <reiballa@gmail.com>
2022-04-26 13:29:09 +0200

    new modal

--- Frontend/src/components/Modal/index.jsx
+++ Frontend/src/components/Modal/index.jsx
@@ -1,49 +1,44 @@
-import { Button, Typography } from "@mui/material"
-import { Box } from "@mui/system"
-import ReactDOM from "react-dom"
-import { colors } from "shared/theme/colors"
+import { Dialog, DialogTitle } from "@mui/material"
 
 /**
  * Modal
- * @param message the Modal bold title message 
  * @param isOpen boolean state passed as prop, if false returns null
- * @param onClose function for Close button onClick event
- * @param children react children component
+ * @param children mui dialog title / dialog content / dialog actions children component
  */
-const Modal = ({ 
-  message, isOpen, onClose, children
+const Modal = ({
+  isOpen, children
 }) => {
-  if(!isOpen) return null
-  return ReactDOM.createPortal(
-    <Box sx={{ overflow: "hidden" }}>
-      <Box sx={{ 
-        position: "absolute", top: "0", left: "0",
-        width: "100vw", height: "100vw",
-        backgroundColor: "rgba(0, 0, 0, 0.6)",
-        zIndex: "20",
-        overflow: "hidden"
-      }}/>
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          backgroundColor: "white",
-          p: "2em",
+  return (
+    <Dialog
+      open={isOpen}
+      onClose={() => setOpen(false)}
+      scroll="paper"
+      aria-labelledby="scroll-dialog-title"
+      aria-describedby="scroll-dialog-description"
+      PaperProps={{
+        sx: {
           borderRadius: "20px",
-          boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.2), 0px 0px 1px rgba(0, 0, 0, 0.3)",
-          zIndex: "100"
-        }}
-        >
-        <Typography fontSize="2em" fontWeight="bold">{message}</Typography>
-        <Box sx={{ height: "1px", width: "100%", backgroundColor: colors.neutral[400], margin: "0.2em 0 1em 0" }}/>
-        {children}
-        {/* TODO Replace with new buttons */}
-        <Button onClick={onClose}>Close</Button>
-      </Box>
-    </Box>
-  , document.body)
+          p: "1em"
+        }
+      }}
+    >
+      {children} 
+    </Dialog>
+  )
+}
+
+export const ModalTitle = ({ children }) => {
+  return (
+    <DialogTitle id="scroll-dialog-title"
+      sx={{
+        fontSize: "1.7em",
+        fontWeight: "bold",
+        p: "0.5em"
+      }}
+    >
+      {children}
+    </DialogTitle>
+  )
 }
 
 export default Modal
--- Frontend/src/views/Dashboard/index.jsx
+++ Frontend/src/views/Dashboard/index.jsx
@@ -1,11 +1,12 @@
-import { Container, Stack } from '@mui/material';
-import React, { useCallback } from 'react';
+import { Stack, Container } from '@mui/material';
+import { useCallback } from 'react';
 import Uploader from 'components/Uploader';
 import StatusQueue from 'components/StatusQueue';
 import styles from './dashboard.module.css';
 
 function Dashboard({ sidebarShown }) {
   const paddingL = useCallback(() => (sidebarShown ? '100px' : '350px'), [sidebarShown]);
+
   return (
     <Stack
       direction="column"
