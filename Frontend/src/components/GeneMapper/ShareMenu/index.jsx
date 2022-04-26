814c5a2288e81cbbab7be0a8fe0b8b54b6b4c80f
Lennart Keller <44754405+lennart-keller@users.noreply.github.com>
2022-04-26 12:38:59 +0200

    add option to copy url

--- Frontend/src/components/GeneMapper/ShareMenu/index.jsx
+++ Frontend/src/components/GeneMapper/ShareMenu/index.jsx
@@ -1,20 +1,25 @@
 import TwitterIcon from '@mui/icons-material/Twitter';
 import EmailIcon from '@mui/icons-material/Email';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { IconButton } from '@mui/material';
 import React from 'react';
 import { colors } from 'shared/theme/colors';
 
 const mailIconColor = colors.neutral['600'];
+const copyIconColor = colors.neutral['600'];
 const twitterIconColor = '#00aced';
 
 function ShareMenu({ projectName, url }) {
   return (
     <>
-      <IconButton aria-label="mail" href={`mailto:?subject=${encodeURIComponent(projectName)}&body=${url}`}>
-        <EmailIcon sx={{ color: mailIconColor }} />
-      </IconButton>
       <IconButton href={`https://twitter.com/share?ref_src=twsrc%5Etfw&url=${url}`} target="_blank">
         <TwitterIcon sx={{ color: twitterIconColor }} />
+      </IconButton>
+      <IconButton href={`mailto:?subject=${encodeURIComponent(projectName)}&body=${url}`}>
+        <EmailIcon sx={{ color: mailIconColor }} />
+      </IconButton>
+      <IconButton onClick={() => navigator.clipboard.writeText(url)}>
+        <ContentCopyIcon sx={{ color: copyIconColor }} />
       </IconButton>
       <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" />
     </>
