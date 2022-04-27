8510e62225e0365598e3c54fa1f81d2c1a30957c
Aleksandra Topalova <aleksandra.i.topalova@gmail.com>
2022-04-27 21:18:43 +0200

    Fixing zoom

--- Frontend/src/components/Visualization/src/newZoom.js
+++ Frontend/src/components/Visualization/src/newZoom.js
@@ -1,31 +1,52 @@
 import * as d3 from "d3";
 import * as cons from "./constants";
 
-export const zoomInN = () => {
-    d3.select('svg')
-      .transition()
-      .call(zoomM.scaleBy, 2);
-}
+// export const zoomInN = () => {
+//     d3.select('svg')
+//       .transition()
+//       .call(zoomM.scaleBy, 2);
+// }
   
+// export const zoomOutN = () => {
+//     d3.select('svg')
+//       .transition()
+//       .call(zoomM.scaleBy, 0.5);
+// }
+
+// let zoomM = 
+// d3.zoom()
+// .scaleExtent([1, 16])
+// .translateExtent([[0, 0], [cons.height, cons.width]])
+// .on('zoom', handleZoom);
+
+// function handleZoom(e) {
+//   select('svg g')
+//   .attr('transform', e.transform);
+// }
+
+// export const resetZoom = () => {
+//     select('svg')
+//        .transition()
+//        .call(zoomM.scaleTo, 1);
+//    }
+
+let zoom = d3.zoom()
+// .scaleExtent([1, 16])
+// .translateExtent([[0, 0], [500, 500]])
+.on("zoom", function () {
+svg.attr("transform", d3.event.transform)
+});
+
+export const panZoom = () => {
+  d3.select("svg")
+  .call(zoom
+    )
+}
+
+export const zoomInN = () => {
+  zoom.scaleBy("svg".transition(), 2);
+}
+
 export const zoomOutN = () => {
-    d3.select('svg')
-      .transition()
-      .call(zoomM.scaleBy, 0.5);
+  zoom.scaleBy("svg".transition(), 0.5);
 }
-
-let zoomM = 
-d3.zoom()
-.scaleExtent([1, 16])
-.translateExtent([[0, 0], [cons.height, cons.width]])
-.on('zoom', handleZoom);
-
-function handleZoom(e) {
-  select('svg g')
-  .attr('transform', e.transform);
-}
-
-export const resetZoom = () => {
-    select('svg')
-       .transition()
-       .call(zoomM.scaleTo, 1);
-   }
