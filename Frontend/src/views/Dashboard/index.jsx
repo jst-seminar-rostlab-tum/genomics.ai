fa52d3bdadafaa7df077be2d606ce584e1c496fe
Jiong Liu <ge65fed@mytum.de>
2022-04-28 15:14:28 +0200

    remove test

--- Frontend/src/views/Dashboard/index.jsx
+++ Frontend/src/views/Dashboard/index.jsx
@@ -4,21 +4,7 @@ import Uploader from 'components/Uploader';
 import StatusQueue from 'components/StatusQueue';
 import styles from './dashboard.module.css';
 
-import { TabGroup } from 'components/Tab'
-import {ModelCard} from 'components/Cards/ModelCard'
-import { useState } from 'react'
-import {Box} from '@mui/material'
-
-const x=(<Box sx={{position: "relative", padding: "10px", display: "flex", flexDirection: "column", justifyContent: "left", gap: "5px"}}>
-<ModelCard title="model 1" description="asdfasdfasdfasdfasdf"/>
-<ModelCard title="model 1" description="asdfasdfasdfasdfasdf"/>
-<ModelCard title="model 1" description="asdfasdfasdfasdfasdf"/>
-<ModelCard title="model 1" description="asdfasdfasdfasdfasdf"/></Box>
-)
-
 function Dashboard({ sidebarShown }) {
-  const [value, setValue] = useState(0)
-
   const paddingL = useCallback(() => (sidebarShown ? '100px' : '350px'), [sidebarShown]);
   return (
     <Stack
@@ -47,7 +33,6 @@ function Dashboard({ sidebarShown }) {
         </Container>
       </Stack>
       
-      <TabGroup value={value} setValue={setValue} width="400px" height="400px" tabsInfo={[{label: "tab1", additionalContent: x},{label: "tab2"},{label: "tab3"}]} />
     </Stack>
   );
 }
