953ab5f6b390f99ee39b041465418f040f8561ab
rei <reiballa@gmail.com>
2022-04-30 15:37:03 +0200

    deleting LandingPage

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,14 +4,14 @@ import { ThemeProvider } from '@mui/material';
 import {
   Route, HashRouter as Router, Switch, Redirect,
 } from 'react-router-dom';
-import HomePage from './views/LandingPage/Home';
-import About from './views/LandingPage/About';
-import Docs from './views/LandingPage/Docs';
+import HomePage from './views/Home';
+import About from './views/About';
+import Docs from './views/Docs';
 import Contact from './views/LandingPage/Contact';
 import DashboardContent from './components/DashboardContent';
 import { guardedPage } from './shared/utils/common/utils';
 import VisualizationPage from './views/VisualizationPage';
-import PasswordResetPage from './views/LandingPage/PasswordResetPage';
+import PasswordResetPage from './views/PasswordResetPage';
 import { theme } from "./shared/theme/theme"
 import Explore from "./views/Explore/index.jsx"
 
