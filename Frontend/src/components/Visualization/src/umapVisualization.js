6bc68230cca51faf4c30f7ce3997f0f11575a3a2
Aleksandra Topalova <aleksandra.i.topalova@gmail.com>
2022-04-30 20:41:37 +0200

    Tooltip done, coloring bug fixed, zoom centered

--- Frontend/src/components/Visualization/src/umapVisualization.js
+++ Frontend/src/components/Visualization/src/umapVisualization.js
@@ -1,213 +1,270 @@
 import * as d3 from "d3";
 import * as cons from "./constants";
-import {zoomM, zoomInN, zoomOutN} from "./newZoom"
+import { zoomM } from "./newZoom";
+import "./tooltip.css";
+
 //TODO: Fix constants
 //TODO: Convert x and y to float beforehand before to_csv
 //TODO: Add a ref attribute
 //TODO: Refactor coloring functions
 
+//
+//Bugs:
+//AttrAdding doesn't work properly
+//Cells are overwritten - *4
+//
 
-const getMin = (w,h) =>
-{
-  return d3.min([h,w]);
+const getMin = (w, h) => {
+  return d3.min([h, w]);
 }
 
- //Hide (for every possible attribute)
- const queryAfter = (cells, category, value) =>{
-    cells
+//Hide (for every possible attribute)
+const queryAfter = (cells, category, value) => {
+  cells
     .style("opacity", d => {
-      if (d[category] == value){
+      if (d[category] == value) {
         return 0;
       }
-      else{
+      else {
         return cons.originalOpacity;
       }
     })
-  
-  }
-  
-  //Show
-  const queryBefore = (cells) =>{
-    cells = cells
+
+}
+
+//Show
+const queryBefore = (cells) => {
+  cells = cells
     .style("opacity", cons.originalOpacity)
+}
+
+//Reference before and after
+
+
+const listColoringDomain = (data, mode) => {
+  let coloringDomain = data.map(x => x[mode]).filter((x, i, a) => a.indexOf(x) == i);
+  return coloringDomain;
+}
+
+//Create a color scale based on the chosen mode and its values
+const setColoring = (mode, data) => {
+  let colorScale;
+  if (!parseFloat(data[0][mode])) {
+    let colorDomain = listColoringDomain(data, mode).sort();
+    colorScale =
+      d3.scaleOrdinal()
+        .domain(colorDomain)
+        .range(cons.colors.slice(0, colorDomain.length));
+
+  }
+  else {
+    var colorDomain = data.map(d => parseFloat(d[mode]));
+    colorScale =
+      d3.scaleLinear()
+        .domain([d3.min(colorDomain), d3.max(colorDomain)])
+        .range(cons.gradientColors);
+
+  }
+  return colorScale;
+}
+
+const addGroup = (svg, id) => {
+  return svg.append('g').attr('id', `${id}`);
+};
+
+//TODO: Fix
+const attrAdding = (cells, attrDom) => {
+  for (let i = 0; i < attrDom.length; i++) {
+    var att = attrDom[i];
+    cells
+      .attr(att, (d) => { return d[att] });
   }
 
-  //Reference before and after
-  
+}
 
-  const listColoringDomain = (data, mode) => {
-    let coloringDomain = data.map(x => x[mode]).filter((x, i, a) => a.indexOf(x) == i);
-    return coloringDomain;
- }
+//Get the possible color modes with their values
+const getColoringModes = (data, colorScale) =>
+  Object.assign({},
+    ...(Object.keys(data[0])
+      .filter(d => (d !== "x" && d !== "y" && d !== ""))
+      .map(d => {
+        var a = {};
+        if (parseFloat(data[0][d])) {
+          const max = d3.max(data.map(val => parseFloat(val[d])));
+          const min = d3.min(data.map(val => parseFloat(val[d])));
+          a[d] = Object.assign({}, ...listColoringDomain(data, d).map(val => parseFloat(val)).filter(val => val == max || val == min).sort((a, b) => a - b)
+            .map((d, i) => {
+              const obj = new Object();
+              obj[d] = cons.gradientColors[i];
+              return obj;
+            }));
+          return a;
+        }
+        const colorDomain = listColoringDomain(data, d).sort()
+        .map((d, i) => {
+          const obj = new Object();
+          obj[d] = cons.colors[i];
+          return obj;
+        });
+        console.log(colorDomain);
+        a[d] = Object.assign({},
+          ...colorDomain)
+        return a;
+      })));
 
-  //Create a color scale based on the chosen mode and its values
-  const setColoring = (mode, data) => {
-    if(!parseFloat(data[0][mode]))
-    {
-      let colorDomain = listColoringDomain(data, mode);
-      var colorScale = 
-      d3.scaleOrdinal()
-      .domain(...colorDomain)
-      .range(cons.colors.slice(0,colorDomain.length));
+export class UmapVisualization2 {
+
+
+  constructor(container, data) {
+    d3.select(container).selectAll("*").remove();
+    this.svg = d3.select(container).append('svg');
+    this.gCells = addGroup(this.svg, 'cells');
+    this.gLabels = addGroup(this.svg, 'labels');
+    this.coloringModes = getColoringModes(data);
+    this.tooltip = d3.select(container).append("div");
+    this.mode = undefined;
+    this.data = data;
+  };
+
+  //Set a color mode
+  setColorMode(mode) {
+    const colorScale = setColoring(mode, this.data);
+    if (this.cells != null) {
+      this.cells = this.cells.style("fill", (d) => colorScale(d[mode]));
     }
-    else{
-      var colorDomain = data.map(d => parseFloat(d[mode]));
-      var colorScale = 
-      d3.scaleLinear()
-      .domain([d3.min(colorDomain),d3.max(colorDomain)])
-      .range(cons.gradientColors);
-    
-    }
+    this.mode = mode;
     return colorScale;
   }
 
-  const addGroup = (svg, id) => {
-    return svg.append('g').attr('id', `${id}`);
-  };
+  //Sizing and resizing the cells
+  resize(w, h) {
+    const min = getMin(w, h);
+    const data = this.data;
 
-  const attrAdding = (cells, attrDom) =>
-  {
-    for(let i=0; i < attrDom.length; i++){
-      var att = attrDom[i];
-      cells =
-      cells
-      .attr(att, (d) => d[att]);
-    }
-    
+    const xScaleHelper = d3.scaleLinear()
+      .domain([d3.min(data.map(d => parseFloat(d.x))), d3.max(data.map(d => parseFloat(d.x)))])
+      .range([cons.margin, min - cons.margin]);
+
+    const translate = (w - cons.margin - (xScaleHelper(d3.max(data.map(d => parseFloat(d.x)))))) / 2;
+
+    //Scales
+    const xScale = d3.scaleLinear()
+      .domain([d3.min(data.map(d => parseFloat(d.x))), d3.max(data.map(d => parseFloat(d.x)))])
+      .range([translate, min - cons.margin + translate]);
+
+    const yScale = d3.scaleLinear()
+      .domain([d3.min(data.map(d => parseFloat(d.y))), d3.max(data.map(d => parseFloat(d.y)))])
+      .range([cons.margin, min - cons.margin]);
+
+    this.cells =
+      this.cells
+        .attr("cx", d => xScale(parseFloat(d.x)))
+        .attr("cy", d => yScale(parseFloat(d.y)))
+
+
+    return [xScale, yScale];
   }
 
-  //Get the possible color modes with their values
-  const getColoringModes = (data) =>
-  Object.assign({},
-    ...(Object.keys(data[0])
-    .filter(d => (d != "x" && d != "y" && d != ""))
-    .map(d =>{ var a = {};
-                if (parseFloat(data[0][d]))
-                {
-                  let max = d3.max(data.map(val => parseFloat(val[d])));
-                  let min = d3.min(data.map(val => parseFloat(val[d])));
-                  a[d] = Object.assign({}, ...listColoringDomain(data,d).map(val => parseFloat(val)).filter( val => val == max || val == min).sort((a,b) => a - b)
-                  .map((d,i) => {
-                  const obj = new Object();
-                  obj[d] = cons.gradientColors[i];
-                  return obj;}));
-                  return a; 
-                }
-                a[d] = Object.assign({},
-                ...listColoringDomain(data,d)
-                .map((d,i) => {
-                  const obj = new Object();
-                  obj[d] = cons.colors[i];
-                  return obj;
-                }))
-              return a;} )));
+  //Constructing the svg
+  async render(w, h) {
 
-  export class UmapVisualization2 {
+    const data = this.data; //dataUnpacked;
+
+    const min = getMin(w, h);
+
+    const r = 0.003 * min;
+
+    //svg
+    this.svg
+      .attr("width", w)
+      .attr("height", h)
+
+    //Circle cells
+    this.cells = this.gCells.selectAll("*").remove();
+
+    this.cells = this.gCells
+      .selectAll("cell")
+      .data(data)
+      .enter()
+      .append("circle")
+      //  .attr("cx", d => xScale(parseFloat(d.x)))
+      //  .attr("cy", d => yScale(parseFloat(d.y)))
+      .attr("r", r)
+      .style("fill", "black")
+      .style("opacity", cons.originalOpacity)
 
 
-    constructor(container, data) {
-      d3.select(container).selectAll("*").remove();
-      this.svg = d3.select(container).append('svg');
-      this.label = this.svg.append("label");
-      this.gCells = addGroup(this.svg, 'cells');
-      this.gLabels = addGroup(this.svg, 'labels');
-      this.coloringModes = getColoringModes(data);
-      this.mode = null;
-      this.data = data;
-    };
+    //Addiding
+    const [xScale, yScale] = this.resize(w, h);
 
-    //Set a color mode
-    setColorMode(mode) {
-      const colorScale = setColoring(mode, this.data);
-      if (this.cells != null){
-        this.cells = this.cells.style("fill", (d)=> colorScale(d[mode]));
-      }
-      this.mode = mode;
-    }
 
-    //Sizing and resizing the cells
-    resize(w, h) {
-      const min = getMin(w,h);
-      const data = this.data;
+    //tooltip
+    var tooltip =
+      this.tooltip
+        .attr('class', 'tooltip')
+        .attr('opacity', 1)
+        .style("visibility", "hidden")
 
-      const  xScaleHelper = d3.scaleLinear()
-        .domain([d3.min(data.map(d => parseFloat(d.x))), d3.max(data.map(d => parseFloat(d.x)))])
-        .range([cons.margin, min-cons.margin]);
-      
-      const translate = (w - cons.margin - (xScaleHelper(d3.max(data.map(d => parseFloat(d.x))))))/2;
-
-        //Scales
-      const  xScale = d3.scaleLinear()
-        .domain([d3.min(data.map(d => parseFloat(d.x))), d3.max(data.map(d => parseFloat(d.x)))])
-        .range([translate, min-cons.margin + translate]);
-  
-      const  yScale = d3.scaleLinear()
-        .domain([d3.min(data.map(d => parseFloat(d.y))), d3.max(data.map(d => parseFloat(d.y)))])
-        .range([cons.margin, min-cons.margin]);
-
-      this.cells = 
+    const _this = this;
+    this.cells =
       this.cells
-      .attr("cx", d => xScale(parseFloat(d.x)))
-      .attr("cy", d => yScale(parseFloat(d.y)))
-
-
-      return [xScale, yScale];
-    }
-
-    //Constructing the svg
-    async render(w, h){
-      
-        const data = this.data; //dataUnpacked;
-        
-        const min = getMin(w,h);
-
-        const r = 0.003*min;
-
-         //svg
-         this.svg
-         .attr("width", w)
-         .attr("height", h)
-         
-         //Circle cells
-         this.cells = this.gCells.selectAll("*").remove();
-
-         this.cells = this.gCells
-         .selectAll("cell")
-         .data(data)
-         .enter()
-         .append("circle")
-        //  .attr("cx", d => xScale(parseFloat(d.x)))
-        //  .attr("cy", d => yScale(parseFloat(d.y)))
-         .attr("r", r)
-         .style("fill", (d) => {
-           if (this.mode != null){
-             const colorScale = setColoring(this.mode, this.data);
-             return colorScale(d[this.mode]);
-           }
-           return cons.fill;
-          })
-         .style("opacity", cons.originalOpacity)
-         
-         //Adding attributes from the different categories for each cell
-         const attrDom = Object.keys(this.coloringModes)
-         attrDom.push("x", "y");
-         attrAdding(this.cells, attrDom);
-
-         this.resize(w,h);
-         
-         //Pan and mouse zoom
-         this.svg
-            .attr('class', 'mouse-capture')
-            .attr('x', -w)
-            .attr('y', -h)
-            .attr('width', w)
-            .attr('height', h)
-            .style('fill', 'white')
-            .lower()
-            .call(zoomM);
+        .on("mouseover", function (m, d) { //getter bigger on hover
+          d3.select(this)
+          .transition()
+          .duration(100)
+          .attr('r', r * 1.5)
+          .style("stroke", "black")
+          .style("opacity", 1)
           
+          // console.log(_this.mode);
+          // const att = d.cell_type + "<br>" + d.batch //? mode : "No information.";
+          // console.log(d);
+          if (!_this.mode) return;
+          const att = d[_this.mode];
+          const xPos = m.screenX + 10;
+          const yPos = m.screenY - 80;
+          
+          tooltip
+            .style("visibility", "visible")
+          tooltip
+            .attr('font-family', 'ui-sans-serif')
+            .html(att)
+            .attr('font-weight', 600)
+            .attr('font-style', 'italic')
+            .style("top", `${yPos}px`)
+            .style("left", `${xPos}px`)
+        })
+        .on("mouseout", function () {
+          tooltip.style("visibility", "hidden");
+          d3.select(this)
+            .transition()
+            .duration(100)
+            .attr('r', r)
+            .style("stroke", "none")
+            .style("opacity", 0.8)
+        })
+    // .on('click', function (d, i){
+    //     const showMore = document.getElementById("moreVisul");
+    //     if(showMore.style.display === 'none') {
+    //         showMore.style.display = 'block';
+    //     } else {
+    //         showMore.style.display = 'none';
+    //     }
+    // });
 
-     }
 
-    }
- 
+    //Pan and mouse zoom
+    this.svg
+      .attr('class', 'mouse-capture')
+      .attr('x', -w)
+      .attr('y', -h)
+      .attr('width', w)
+      .attr('height', h)
+      .style('fill', 'white')
+      .lower()
+      .call(zoomM);
+  
+
+  }
+
+}
