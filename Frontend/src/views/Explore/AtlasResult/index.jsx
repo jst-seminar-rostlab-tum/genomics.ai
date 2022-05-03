67edececf929d352f095cabc25c50c7f8ecfa8f9
rei <reiballa@gmail.com>
2022-05-03 23:56:05 +0200

    remove test csv

--- Frontend/src/views/Explore/AtlasResult/index.jsx
+++ Frontend/src/views/Explore/AtlasResult/index.jsx
@@ -1,15 +1,14 @@
 import { Box } from "@mui/system";
-import csv from "assets/test_file2.csv";
 import ResultVisualization from "components/GeneMapper/ResultVisualization";
 import { useParams } from "react-router-dom";
 
 export default function() {
 
-  const { id } = useParams()
+  const { id } = useParams
 
   return (
     <Box>
-      <ResultVisualization dataUrl={csv} onlyUmap/>
+      <ResultVisualization dataUrl={path} onlyUmap/>
     </Box>
   )
 }
