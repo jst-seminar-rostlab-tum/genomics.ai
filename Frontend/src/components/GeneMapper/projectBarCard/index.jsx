86e1d63fd7d8b6ee727da74b53175c042482032a
Xufan Lu <xufanlu.joanna@gmail.com>
2022-05-03 09:50:14 +0200

    add card aktion

--- Frontend/src/components/GeneMapper/projectBarCard/index.jsx
+++ Frontend/src/components/GeneMapper/projectBarCard/index.jsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import {
-  Box, FormControl, InputLabel, MenuItem, Modal, Select, Stack,
+  Box, CardActionArea, FormControl, InputLabel, MenuItem, Modal, Select, Stack,
 } from '@mui/material';
 import CircleIcon from '@mui/icons-material/Circle';
 import { useHistory } from 'react-router-dom';
@@ -15,13 +15,19 @@ export default function ProjectBarCard({ projectId, name, status }) {
   const [typographyColor, setTypographyColor] = React.useState(status === 'UPLOAD FAILED' ? 'red' : 'black');
   const [addTeam, setAddTeam] = React.useState(false);
   const [team, setTeam] = React.useState('');
+  const [foldOut, setfoldOut] = React.useState(false);
   const handleOpen = () => setAddTeam(true);
   const handleClose = () => setAddTeam(false);
   const handleChange = (event) => {
     setTeam(event.target.value);
     window.localStorage.setItem('Team', event.target.value);
   };
-
+  const handleClickCard = () => {
+    setfoldOut(true);
+  };
+  //   const handleDelete = () => {
+  // set
+  //   };
   React.useEffect(() => {
     if (window.localStorage.getItem('Team')) { setTeam(window.localStorage.getItem('Team')); }
   }, []);
@@ -31,102 +37,105 @@ export default function ProjectBarCard({ projectId, name, status }) {
       marginTop: '5', marginBottom: '0.5em', borderStyle: 'solid', borderColor: '#C8C8C8', borderWidth: '0.1px',
     }}
     >
-      <Stack direction="row" sx={{ justifyContent: 'space-between', height: '56px' }}>
-        <Stack
-          direction="row"
-          spacing={4}
-          sx={{ width: '50%', alignItems: 'center' }}
-        >
+      <CardActionArea onClick={handleClickCard}>
+        <Stack direction="row" sx={{ justifyContent: 'space-between', height: '56px' }}>
 
-          <CircleIcon sx={{
-            fontSize: 30, marginLeft: '3%', color,
+          <Stack
+            direction="row"
+            spacing={4}
+            sx={{ width: '50%', alignItems: 'center' }}
+          >
+
+            <CircleIcon sx={{
+              fontSize: 30, marginLeft: '3%', color,
+            }}
+            />
+            <Typography>
+              {name}
+            </Typography>
+            <Typography sx={{ color: typographyColor }}>
+              {status}
+            </Typography>
+            <Typography sx={{ color: typographyColor }}>
+              {team}
+            </Typography>
+          </Stack>
+
+          <Box sx={{
+            p: 0.1, bgcolor: 'background.paper', borderRadius: 3, width: 'flex', mr: 3, display: 'flex', m: 2, alignItems: 'center',
           }}
-          />
-          <Typography>
-            {name}
-          </Typography>
-          <Typography sx={{ color: typographyColor }}>
-            {status}
-          </Typography>
-          <Typography sx={{ color: typographyColor }}>
-            {team}
-          </Typography>
-        </Stack>
-        <Box sx={{
-          p: 0.1, bgcolor: 'background.paper', borderRadius: 3, width: 'flex', mr: 3, display: 'flex', m: 2, alignItems: 'center',
-        }}
-        >
-          <Button
-            variant="outlined"
+          >
+            <Button
+              variant="outlined"
             // size="small"
-            sx={{
-              borderRadius: 100,
-              mr: 2,
-            }}
-            style={{ textTransform: 'none' }}
-            onClick={handleOpen}
-          >
-            Add To Team
-          </Button>
-          <Modal
-            open={addTeam}
-            OnClose={handleClose}
-          >
-            <Box sx={{
-              backgroundColor: 'background.paper',
-              position: 'absolute',
-              top: '30%',
-              left: '50%',
-              p: '5%',
-              borderRadius: 5,
-              transform: 'translate(-50%, -50%)',
-
-            }}
+              sx={{
+                borderRadius: 100,
+                mr: 2,
+              }}
+              style={{ textTransform: 'none' }}
+              onClick={handleOpen}
             >
-              <Typography variant="h6" sx={{ paddingBottom: 1 }}>
-                Select A Team
-              </Typography>
-              <div>
-                <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-                  <InputLabel id="demo-simple-select-standard-label">Team</InputLabel>
-                  <Select
-                    labelId="demo-simple-select-standard-label"
-                    id="demo-simple-select-standard"
-                    value={team}
-                    onChange={handleChange}
-                    label="Team"
-                  >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    <MenuItem value="Team 1">Team 1</MenuItem>
-                    <MenuItem value="Team 2">Team 2</MenuItem>
-                    <MenuItem value="Team 3">Team 3</MenuItem>
-                  </Select>
-                </FormControl>
-              </div>
-              <Button sx={{ paddingTop: 4 }} size="small" onClick={() => setAddTeam(false)}>Confirm</Button>
-              <Button sx={{ paddingTop: 4 }} size="small" onClick={() => setAddTeam(false)}>Close</Button>
-            </Box>
-          </Modal>
+              Add To Team
+            </Button>
+            <Modal
+              open={addTeam}
+              OnClose={handleClose}
+            >
+              <Box sx={{
+                backgroundColor: 'background.paper',
+                position: 'absolute',
+                top: '30%',
+                left: '50%',
+                p: '5%',
+                borderRadius: 5,
+                transform: 'translate(-50%, -50%)',
 
-          <Button
-            variant="contained"
-            color="secondary"
-            sx={{
-              borderRadius: 100,
-              mr: 2,
-            }}
-            style={{ textTransform: 'none' }}
-            onClick={() => history.push(`./genemapper/result/${projectId}`)}
-            disabled={status !== 'DONE'}
-          >
-            See Results
-          </Button>
-          <Button>
-            <DeleteOutlineIcon />
-          </Button>
-          {/* <CustomButton
+              }}
+              >
+                <Typography variant="h6" sx={{ paddingBottom: 1 }}>
+                  Select A Team
+                </Typography>
+                <div>
+                  <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                    <InputLabel id="demo-simple-select-standard-label">Team</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-standard-label"
+                      id="demo-simple-select-standard"
+                      value={team}
+                      onChange={handleChange}
+                      label="Team"
+                    >
+                      <MenuItem value="">
+                        <em>None</em>
+                      </MenuItem>
+                      <MenuItem value="Team 1">Team 1</MenuItem>
+                      <MenuItem value="Team 2">Team 2</MenuItem>
+                      <MenuItem value="Team 3">Team 3</MenuItem>
+                    </Select>
+                  </FormControl>
+                </div>
+                <Button sx={{ paddingTop: 4 }} size="small" onClick={() => setAddTeam(false)}>Confirm</Button>
+                <Button sx={{ paddingTop: 4 }} size="small" onClick={() => setAddTeam(false)}>Close</Button>
+              </Box>
+            </Modal>
+
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{
+                borderRadius: 100,
+                mr: 2,
+              }}
+              style={{ textTransform: 'none' }}
+              onClick={() => history.push(`./genemapper/result/${projectId}`)}
+              disabled={status !== 'DONE'}
+            >
+              See Results
+            </Button>
+            <Button>
+              <DeleteOutlineIcon />
+            </Button>
+            {/* <CustomButton
             type="primary"
             sx={{
               outerHeight: '70%',
@@ -135,8 +144,10 @@ export default function ProjectBarCard({ projectId, name, status }) {
             Create Mapping
           </CustomButton> */}
 
-        </Box>
-      </Stack>
+          </Box>
+        </Stack>
+
+      </CardActionArea>
     </Card>
   );
 }
