661ca04967a4a8c234076c33d0f52a6a53761cf0
Evan Christopher <evanchristopher1907@gmail.com>
2022-05-04 09:23:02 +0200

    Style improvements and code cleanup

--- Frontend/src/components/GeneMapper/TabCard/index.jsx
+++ Frontend/src/components/GeneMapper/TabCard/index.jsx
@@ -26,36 +26,34 @@ export const TabCard = ({ width, height, data, handleOnClick, selected}) => {
     // }, [color])
 
     return (
-        <Box sx={{ width: {width}, height: {height}, backgroundColor: 'white', borderRadius: "0.625rem", marginTop: '0.5em' }}>
-            <CardActionArea disableTouchRipple onClick={handleOnClick}>
-                <Box 
-                    sx={ selected ? {
-                        boxShadow: "0px 0px 2px rgba(0,0,0, 0.15)", 
-                        p: "0.5em", 
-                        borderRadius: "0.625rem",
-                        backgroundColor: colors.primary[300],
-                        color: 'white'
-                    }
-                    : { 
+        <Box onClick={handleOnClick} sx={{ width: {width}, height: {height}, backgroundColor: 'white', borderRadius: "0.625rem", marginTop: '0.5em' }}>
+            <Box 
+                sx={ selected ? {
                     boxShadow: "0px 0px 2px rgba(0,0,0, 0.15)", 
                     p: "0.5em", 
                     borderRadius: "0.625rem",
-                    backgroundColor: 'white',
-                    ":hover": {
-                        color: colors.primary,
-                        ':hover': { backgroundColor: colors.primary[300], transition: '0.4s', color: 'white' },
-                        ':focus': { backgroundColor: colors.primary[300], transition: '0.4s', color: 'white' },
-                        ':disabled': { backgroundColor: '#EBEFFF', transition: '0.4s', color: colors.primary[600] }
-                    }}}
-                >
-                    <Stack direction="row" spacing={4} p='0.1em' pl='0.3em'>
-                        {/* <CircleIcon sx={{ fontSize: 18, marginLeft: '3%', marginTop: '0.2em', color }} /> */}
-                        <Typography variant='body1'>
-                            {data.name}
-                        </Typography>
-                    </Stack>
-                </Box>
-            </CardActionArea>
+                    backgroundColor: colors.primary[300],
+                    color: 'white'
+                }
+                : { 
+                boxShadow: "0px 0px 2px rgba(0,0,0, 0.15)", 
+                p: "0.5em", 
+                borderRadius: "0.625rem",
+                backgroundColor: 'white',
+                ":hover": {
+                    color: colors.primary,
+                    ':hover': { backgroundColor: colors.primary[300], transition: '0.4s', color: 'white' },
+                    ':focus': { backgroundColor: colors.primary[300], transition: '0.4s', color: 'white' },
+                    ':disabled': { backgroundColor: '#EBEFFF', transition: '0.4s', color: colors.primary[600] }
+                }}}
+            >
+                <Stack direction="row" spacing={4} p='0.1em' pl='0.3em'>
+                    {/* <CircleIcon sx={{ fontSize: 18, marginLeft: '3%', marginTop: '0.2em', color }} /> */}
+                    <Typography variant='body1'>
+                        {data.name}
+                    </Typography>
+                </Stack>
+            </Box>
         </Box>
     );
 }
