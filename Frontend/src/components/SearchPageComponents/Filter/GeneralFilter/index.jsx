e24724b5afd2cac92e141f13bce1caf38298931c
Nikita <nikita.charush@gmail.com>
2022-05-13 15:04:48 +0200

    Integrated current Atlasfilter in SearchPage

--- Frontend/src/components/SearchPageComponents/Filter/GeneralFilter/index.jsx
+++ Frontend/src/components/SearchPageComponents/Filter/GeneralFilter/index.jsx
@@ -3,16 +3,20 @@ import LabeledSelect from '../LabeledSelect';
 
 // General filter that is needed in all categories
 const GeneralFilter = ({ sortBy, onChange }) => {
+  const defaultValue = 'name';
+
   const sortItems = [
     { label: 'Name', value: 'name' },
     { label: 'Last updated', value: 'updatedAt' },
   ];
 
+  const sortItem = sortItems.find((item) => item.value === sortBy);
+
   return (
     <LabeledSelect
       label="Sort by"
-      value={sortBy}
-      defaultValue="name"
+      value={sortItem && sortItem.value}
+      defaultValue={defaultValue}
       onChange={(event) => onChange('sortBy', event.target.value)}
       items={sortItems}
     />
