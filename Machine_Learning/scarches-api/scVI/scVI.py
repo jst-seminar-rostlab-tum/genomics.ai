eb8dbc8b51397b1cd310a399c82fbacf01db7311
alextopalova <aleksandra.i.topalova@gmail.com>
2022-05-18 15:38:10 +0200

    Redundancy fixing #2

--- Machine_Learning/scarches-api/scVI/scVI.py
+++ Machine_Learning/scarches-api/scVI/scVI.py
@@ -11,10 +11,10 @@ import sys
 import tempfile
 
 
-def get_from_config(configuration, key):
-    if key in configuration:
-        return configuration[key]
-    return None
+# def utils.get_from_config(configuration, key):
+#     if key in configuration:
+#         return configuration[key]
+#     return None
 
 
 # python3.9 scVI.py --input data/pancreas_normalized.h5ad -t -q
@@ -36,8 +36,8 @@ def setup():
 
 
 def pre_process_data(configuration):
-    source_adata = utils.read_h5ad_file_from_s3(get_from_config(configuration, parameters.REFERENCE_DATA_PATH))
-    target_adata = utils.read_h5ad_file_from_s3(get_from_config(configuration, parameters.QUERY_DATA_PATH))
+    source_adata = utils.read_h5ad_file_from_s3(utils.get_from_config(configuration, parameters.REFERENCE_DATA_PATH))
+    target_adata = utils.read_h5ad_file_from_s3(utils.get_from_config(configuration, parameters.QUERY_DATA_PATH))
     source_adata = remove_sparsity(source_adata)
     source_adata.obs["type"] = "reference"
     target_adata.obs["type"] = "query"
@@ -60,29 +60,29 @@ def create_scVI_model(source_adata, target_adata, configuration):
     :param target_adata:
     :return:
     """
-    if get_from_config(configuration, parameters.DEV_DEBUG):
-        print('use_pretrained is ' + str(get_from_config(configuration, parameters.USE_PRETRAINED_SCVI_MODEL)),
+    if utils.get_from_config(configuration, parameters.DEV_DEBUG):
+        print('use_pretrained is ' + str(utils.get_from_config(configuration, parameters.USE_PRETRAINED_SCVI_MODEL)),
               file=sys.stderr)
-    if get_from_config(configuration, parameters.USE_PRETRAINED_SCVI_MODEL):
-        if get_from_config(configuration, parameters.DEV_DEBUG):
+    if utils.get_from_config(configuration, parameters.USE_PRETRAINED_SCVI_MODEL):
+        if utils.get_from_config(configuration, parameters.DEV_DEBUG):
             print('use pretrained scvi model', file=sys.stderr)
         # os.mkdir('scvi_model')
-        # utils.fetch_file_from_s3(get_from_config(configuration, parameters.PRETRAINED_MODEL_PATH), 'assets/scVI/model.pt')
+        # utils.fetch_file_from_s3(utils.get_from_config(configuration, parameters.PRETRAINED_MODEL_PATH), 'assets/scVI/model.pt')
         return get_pretrained_scVI_model(target_adata), None
     else:
-        if get_from_config(configuration, parameters.DEV_DEBUG):
+        if utils.get_from_config(configuration, parameters.DEV_DEBUG):
             print('do not use pretrained scvi model', file=sys.stderr)
         setup_anndata(source_adata, configuration)
         vae = get_model(source_adata, configuration)
-        vae.train(max_epochs=get_from_config(configuration, parameters.SCVI_MAX_EPOCHS),
-                  use_gpu=get_from_config(configuration, parameters.USE_GPU))
-        if get_from_config(configuration, parameters.DEV_DEBUG):
+        vae.train(max_epochs=utils.get_from_config(configuration, parameters.SCVI_MAX_EPOCHS),
+                  use_gpu=utils.get_from_config(configuration, parameters.USE_GPU))
+        if utils.get_from_config(configuration, parameters.DEV_DEBUG):
             try:
                 utils.write_adata_to_csv(vae, source_adata, key='scvi-source-adata-post-first-training.csv')
             except Exception as e:
                 print(e, file=sys.stderr)
         reference_latent = compute_latent(vae, source_adata, configuration)
-        if get_from_config(configuration, parameters.DEV_DEBUG):
+        if utils.get_from_config(configuration, parameters.DEV_DEBUG):
             try:
                 utils.write_adata_to_csv(vae, source_adata, key='scvi-reference-latent-post-first-training.csv')
             except Exception as e:
@@ -91,8 +91,8 @@ def create_scVI_model(source_adata, target_adata, configuration):
         vae.save(tempdir, overwrite=True)
         # TODO check if we need to to this
         print(os.listdir(tempdir), file=sys.stderr)
-        # utils.store_file_in_s3(tempdir + '/model.pt', get_from_config(configuration, parameters.RESULTING_MODEL_PATH))
-        if get_from_config(configuration, parameters.DEV_DEBUG):
+        # utils.store_file_in_s3(tempdir + '/model.pt', utils.get_from_config(configuration, parameters.RESULTING_MODEL_PATH))
+        if utils.get_from_config(configuration, parameters.DEV_DEBUG):
             try:
                 utils.store_file_in_s3(tempdir + '/model.pt', 'scvi-model-after-first-training.pt')
             except Exception as e:
@@ -108,8 +108,8 @@ def setup_anndata(anndata, configuration):
     :param anndata:
     :return:
     """
-    sca.models.SCVI.setup_anndata(anndata, batch_key=get_from_config(configuration, parameters.CONDITION_KEY),
-                                  labels_key=get_from_config(configuration, parameters.CELL_TYPE_KEY))
+    sca.models.SCVI.setup_anndata(anndata, batch_key=utils.get_from_config(configuration, parameters.CONDITION_KEY),
+                                  labels_key=utils.get_from_config(configuration, parameters.CELL_TYPE_KEY))
 
 
 def get_model(anndata, configuration):
@@ -120,11 +120,11 @@ def get_model(anndata, configuration):
     """
     return sca.models.SCVI(
         anndata,
-        n_layers=get_from_config(configuration, parameters.NUMBER_OF_LAYERS),
-        encode_covariates=get_from_config(configuration, parameters.ENCODE_COVARIATES),
-        deeply_inject_covariates=get_from_config(configuration, parameters.DEEPLY_INJECT_COVARIATES),
-        use_layer_norm=get_from_config(configuration, parameters.USE_LAYER_NORM),
-        use_batch_norm=get_from_config(configuration, parameters.USE_BATCH_NORM),
+        n_layers=utils.get_from_config(configuration, parameters.NUMBER_OF_LAYERS),
+        encode_covariates=utils.get_from_config(configuration, parameters.ENCODE_COVARIATES),
+        deeply_inject_covariates=utils.get_from_config(configuration, parameters.DEEPLY_INJECT_COVARIATES),
+        use_layer_norm=utils.get_from_config(configuration, parameters.USE_LAYER_NORM),
+        use_batch_norm=utils.get_from_config(configuration, parameters.USE_BATCH_NORM),
     )
 
 
@@ -136,9 +136,9 @@ def compute_latent(model, adata, configuration):
     :return:
     """
     reference_latent = sc.AnnData(model.get_latent_representation(adata=adata))
-    reference_latent.obs["cell_type"] = adata.obs[get_from_config(configuration, parameters.CELL_TYPE_KEY)].tolist()
-    reference_latent.obs["batch"] = adata.obs[get_from_config(configuration, parameters.CONDITION_KEY)].tolist()
-    sc.pp.neighbors(reference_latent, n_neighbors=get_from_config(configuration, parameters.NUMBER_OF_NEIGHBORS))
+    reference_latent.obs["cell_type"] = adata.obs[utils.get_from_config(configuration, parameters.CELL_TYPE_KEY)].tolist()
+    reference_latent.obs["batch"] = adata.obs[utils.get_from_config(configuration, parameters.CONDITION_KEY)].tolist()
+    sc.pp.neighbors(reference_latent, n_neighbors=utils.get_from_config(configuration, parameters.NUMBER_OF_NEIGHBORS))
     sc.tl.leiden(reference_latent)
     sc.tl.umap(reference_latent)
     # no need to show scatterplot during computation
@@ -163,55 +163,56 @@ def compute_query(pretrained_model, anndata, reference_latent, source_adata, con
         freeze_dropout=True,
     )
     model.train(
-        max_epochs=get_from_config(configuration, parameters.SCVI_QUERY_MAX_EPOCHS),
+        max_epochs=utils.get_from_config(configuration, parameters.SCVI_QUERY_MAX_EPOCHS),
         plan_kwargs=dict(weight_decay=0.0),
         check_val_every_n_epoch=10,
-        use_gpu=get_from_config(configuration, parameters.USE_GPU)
+        use_gpu=utils.get_from_config(configuration, parameters.USE_GPU)
     )
     tempdir = tempfile.mkdtemp()
     model.save(tempdir, overwrite=True)
-    if get_from_config(configuration, parameters.DEV_DEBUG):
+    if utils.get_from_config(configuration, parameters.DEV_DEBUG):
         try:
             utils.store_file_in_s3(tempdir + '/model.pt', 'scvi-model-after-query-training.pt')
         except Exception as e:
             print(e, file=sys.stderr)
     utils.delete_file(tempdir + '/model.pt')
     os.removedirs(tempdir)
-    if get_from_config(configuration, parameters.DEV_DEBUG):
+    if utils.get_from_config(configuration, parameters.DEV_DEBUG):
         try:
             if reference_latent is not None:
                 utils.write_latent_csv(reference_latent, key='reference-latent-post-query-training.csv')
             utils.write_adata_to_csv(model, anndata, key='query-adata-post-query-training.csv',
-                                     cell_type_key=get_from_config(configuration, parameters.CELL_TYPE_KEY),
-                                     condition_key=get_from_config(configuration, parameters.CONDITION_KEY))
+                                     cell_type_key=utils.get_from_config(configuration, parameters.CELL_TYPE_KEY),
+                                     condition_key=utils.get_from_config(configuration, parameters.CONDITION_KEY))
             utils.write_adata_to_csv(model, source_adata, key='source-adata-post-query-training.csv',
-                                     cell_type_key=get_from_config(configuration, parameters.CELL_TYPE_KEY),
-                                     condition_key=get_from_config(configuration, parameters.CONDITION_KEY))
+                                     cell_type_key=utils.get_from_config(configuration, parameters.CELL_TYPE_KEY),
+                                     condition_key=utils.get_from_config(configuration, parameters.CONDITION_KEY))
         except Exception as e:
             print(e, file=sys.stderr)
 
     query_latent = compute_latent(model, anndata, configuration)
-    if get_from_config(configuration, parameters.DEV_DEBUG):
+    if utils.get_from_config(configuration, parameters.DEV_DEBUG):
         try:
             utils.write_latent_csv(query_latent, key='query-latent-post-query-training.csv')
             if reference_latent is not None:
                 utils.write_combined_csv(reference_latent, query_latent, key='combined-latents-after-query.csv')
         except Exception as e:
             print(e, file=sys.stderr)
-    if get_from_config(configuration, parameters.DEBUG):
+    if utils.get_from_config(configuration, parameters.DEBUG):
         utils.save_umap_as_pdf(query_latent, 'data/figures/query.pdf', color=['batch', 'cell_type'])
 
-    # utils.write_latent_csv(query_latent, key=get_from_config(configuration, parameters.OUTPUT_PATH))
-    # utils.write_combined_csv(reference_latent, query_latent, key=get_from_config(configuration, parameters.OUTPUT_PATH))
-    # utils.write_adata_to_csv(model, source_adata, key=get_from_config(configuration, parameters.OUTPUT_PATH),
-    #                              cell_type_key=get_from_config(configuration, parameters.CELL_TYPE_KEY),
-    #                              condition_key=get_from_config(configuration, parameters.CONDITION_KEY))
+    # utils.write_latent_csv(query_latent, key=utils.get_from_config(configuration, parameters.OUTPUT_PATH))
+    # utils.write_combined_csv(reference_latent, query_latent, key=utils.get_from_config(configuration, parameters.OUTPUT_PATH))
+    # utils.write_adata_to_csv(model, source_adata, key=utils.get_from_config(configuration, parameters.OUTPUT_PATH),
+    #                              cell_type_key=utils.get_from_config(configuration, parameters.CELL_TYPE_KEY),
+    #                              condition_key=utils.get_from_config(configuration, parameters.CONDITION_KEY))
     utils.write_full_adata_to_csv(model, source_adata, anndata,
-                                  key=get_from_config(configuration, parameters.OUTPUT_PATH),
-                                  cell_type_key=get_from_config(configuration, parameters.CELL_TYPE_KEY),
-                                  condition_key=get_from_config(configuration, parameters.CONDITION_KEY))
+                                  key=utils.get_from_config(configuration, parameters.OUTPUT_PATH),
+                                  cell_type_key=utils.get_from_config(configuration, parameters.CELL_TYPE_KEY),
+                                  condition_key=utils.get_from_config(configuration, parameters.CONDITION_KEY))
 
     return model
+
 
 
 def compute_full_latent(source_adata, target_adata, model, configuration):
@@ -224,11 +225,12 @@ def compute_full_latent(source_adata, target_adata, model, configuration):
     """
     full_latent = compute_latent(model, source_adata.concatenate(target_adata), configuration)
 
-    if get_from_config(configuration, parameters.DEBUG):
+    if utils.get_from_config(configuration, parameters.DEBUG):
         utils.save_umap_as_pdf(full_latent, 'data/figures/both.pdf', color=['batch', 'cell_type'])
 
     both_path = 'both.csv'
-    utils.write_latent_csv(full_latent, key='both.csv', filename=both_path)
+    
+    utils.write_latent_csv(full_latent, key='both.csv', filename=both_path, drop_colums = dropped_columns)
 
     return full_latent
 
@@ -241,4 +243,4 @@ def compute_scVI(configuration):
     # TODO figure out if we need to do this
     # compute_full_latent(source_adata, target_adata, model)
     # model.save('resulting_model', overwrite=True)
-    # utils.store_file_in_s3('resulting_model/model.pt', get_from_config(configuration, parameters.RESULTING_MODEL_PATH) + '_new')
+    # utils.store_file_in_s3('resulting_model/model.pt', utils.get_from_config(configuration, parameters.RESULTING_MODEL_PATH) + '_new')
