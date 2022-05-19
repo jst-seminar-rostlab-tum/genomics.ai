a6803e9cf1c3c435f437db8c97d77a1095eaa11d
Andreas Resch <andreas@resch.io>
2022-05-19 10:42:10 +0200

    return json if error occurs

--- Machine_Learning/scarches-api/api.py
+++ Machine_Learning/scarches-api/api.py
@@ -3,6 +3,7 @@ import os
 import init as scarches
 from threading import Thread
 from utils import utils, parameters
+
 app = Flask(__name__)
 
 
@@ -11,19 +12,22 @@ def get_from_config(configuration, key):
         return configuration[key]
     return None
 
+
 @app.route('/query', methods=['POST'])
 def query():
-    config = request.get_json(force=True)
-    run_async = get_from_config(config, parameters.RUN_ASYNCHRONOUSLY)
-    if run_async is not None and run_async:
-        actual_config = scarches.merge_configs(config)
-        print("returning")
-        thread = Thread(target=scarches.query, args=(config,))
-        thread.start()
-        return actual_config, 200
-    else:
-        actual_configuration = scarches.query(config)
-        return actual_configuration, 200
+    try:
+        config = request.get_json(force=True)
+        run_async = get_from_config(config, parameters.RUN_ASYNCHRONOUSLY)
+        if run_async is not None and run_async:
+            actual_config = scarches.merge_configs(config)
+            thread = Thread(target=scarches.query, args=(config,))
+            thread.start()
+            return actual_config, 200
+        else:
+            actual_configuration = scarches.query(config)
+            return actual_configuration, 200
+    except Exception as e:
+        return {'error': str(e)}, 500
 
 
 @app.route("/liveness")
