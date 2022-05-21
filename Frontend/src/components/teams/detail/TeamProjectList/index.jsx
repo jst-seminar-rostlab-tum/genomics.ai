ed3fc826925adb05a3f8d4be5f96489f6cc00689
Cem Nasit Sarica <cemnasitsarica@gmail.com>
2022-05-21 15:04:01 +0200

    using new projectbarcard

--- Frontend/src/components/teams/detail/TeamProjectList/index.jsx
+++ Frontend/src/components/teams/detail/TeamProjectList/index.jsx
@@ -4,37 +4,19 @@ import ProjectBarCard from 'components/GeneMapper/projectBarCard';
 import AtlasService from 'shared/services/Atlas.service';
 import ModelService from 'shared/services/Model.service';
 import TeamService from 'shared/services/Team.service';
-import { useSubmissionProgress } from 'shared/context/submissionProgressContext';
-import { MULTIPART_UPLOAD_STATUS, PROJECTS_UPDATE_INTERVAL, statusIsError } from 'shared/utils/common/constants';
 
 function TeamProjectList({ teamId }) {
   const [projects, setProjects] = useState([]);
   const [atlases, setAtlases] = useState([]);
   const [models, setModels] = useState([]);
   const [userTeams, setUserTeams] = useState([]);
-  const [submissionProgress, setSubmissionProgress] = useSubmissionProgress();
 
   useEffect(() => {
     AtlasService.getAtlases().then((data) => setAtlases(data));
     ModelService.getModels().then((data) => setModels(data));
     TeamService.getMyTeams().then((teams) => setUserTeams(teams));
-  }, []);
-
-  useEffect(() => {
     ProjectService.getTeamProjects(teamId).then((data) => setProjects(data));
-    if (submissionProgress.status === MULTIPART_UPLOAD_STATUS.COMPLETE
-      || submissionProgress.status === MULTIPART_UPLOAD_STATUS.CANCELING
-      || statusIsError(submissionProgress.status)) {
-      setSubmissionProgress({
-        status: MULTIPART_UPLOAD_STATUS.IDLE,
-        uploadId: '',
-        chunks: 0,
-        uploaded: 0,
-        remaining: [],
-        uploadedParts: [],
-      });
-    }
-  }, [submissionProgress.status]);
+  }, []);
 
   if (projects.length === 0) return (<span>No projects.</span>);
 
@@ -47,12 +29,7 @@ function TeamProjectList({ teamId }) {
           atlas={atlases.find((atlas) => String(atlas._id) === String(project.atlasId))}
           model={models.find((model) => String(model._id) === String(project.modelId))}
           userTeams={userTeams}
-          addProjectToTeam={() => true}
           handleDelete={() => true}
-          submissionProgress={submissionProgress.uploadId === project.uploadId
-            ? submissionProgress : null}
-          setSubmissionProgress={submissionProgress.uploadId === project.uploadId
-            ? setSubmissionProgress : () => { }}
         />
 
       );
