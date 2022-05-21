a89fa54d6b951148a94cefb67ca21dcfa5e23e64
Cem Nasit Sarica <cemnasitsarica@gmail.com>
2022-05-21 15:33:36 +0200

    Tooltip for not being able to join added

--- Frontend/src/components/teams/detail/TeamUserHeaderRight/index.jsx
+++ Frontend/src/components/teams/detail/TeamUserHeaderRight/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import TeamLeaveButton from 'components/teams/overview/TeamLeaveButton';
 import TeamJoinButton from 'components/teams/detail/TeamJoinButton';
+import { Tooltip } from '@mui/material';
 
 function TeamUserHeaderRight({
   institution, team, user, updateTeam,
@@ -29,14 +30,19 @@ function TeamUserHeaderRight({
     );
   }
 
+  const canJoin = (team.visibility === 'PUBLIC') || (team.visibility === 'BY_INSTITUTION' && institution.memberIds.includes(user._id))
+    || (team.invitedMemberIds.includes(user._id));
+
   return (
-    <TeamJoinButton
-      isDisabled={(team.visibility === 'PRIVATE' && (!team.invitedMemberIds.includes(user._id)))
-        || (team.visibility === 'BY_INSTITUTION' && (!institution.memberIds.includes(user._id) && !institution.adminIds.includes(user._id))
-          && (!team.invitedMemberIds.includes(user._id)))}
-      team={team}
-      onJoin={onJoin}
-    />
+    <Tooltip title={canJoin ? '' : `This team is ${team.visibility.toLowerCase()} and you haven't been invited${team.visibility === 'BY_INSTITUTION' ? " or you're a member of this team's institution" : ''}.`}>
+      <div>
+        <TeamJoinButton
+          isDisabled={!canJoin}
+          team={team}
+          onJoin={onJoin}
+        />
+      </div>
+    </Tooltip>
   );
 }
 
