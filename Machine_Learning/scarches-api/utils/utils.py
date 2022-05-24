1141412622b32c4a4422e4405c85016571c5c7f3
cindyguan28 <64539283+cindyguan28@users.noreply.github.com>
2022-05-24 16:15:00 +0200

    change csv prediction cell type to predicted yes or no

--- Machine_Learning/scarches-api/utils/utils.py
+++ Machine_Learning/scarches-api/utils/utils.py
@@ -9,6 +9,7 @@ from pathlib import Path
 import scanpy
 import fileinput
 from pathlib import Path
+import pandas as pd
 
 
 def write_latent_csv(latent, key=None, filename=tempfile.mktemp(), drop_columns=None):
@@ -25,20 +26,32 @@ def write_latent_csv(latent, key=None, filename=tempfile.mktemp(), drop_columns=
     final = latent.obs.drop(columns=drop_columns)
     final["x"] = list(map(lambda p: p[0], latent.obsm["X_umap"]))
     final["y"] = list(map(lambda p: p[1], latent.obsm["X_umap"]))
+
+    final['predictions'] = final['predictions'].apply(lambda cell_type: prediction_value(cell_type))
     final.to_csv(filename)
+
     if key is not None:
         store_file_in_s3(filename, key)
     return filename
 
 
+def prediction_value(cell_type):
+    if cell_type is None:
+        return "yes"
+    else:
+        return "no"
+
+
 def write_full_adata_to_csv(model, source_adata, target_adata, key=None, filename=tempfile.mktemp(), drop_columns=None,
-                            cell_type_key='', condition_key='', neighbors=8, predictScanvi=False):
+                            unlabeled_category='', cell_type_key='', condition_key='', neighbors=8,
+                            predictScanvi=False):
     adata_full = source_adata.concatenate(target_adata)
-    return write_adata_to_csv(model, adata_full, key, filename, drop_columns, cell_type_key, condition_key, neighbors, predictScanvi)
+    return write_adata_to_csv(model, adata_full, key, filename, drop_columns, unlabeled_category, cell_type_key,
+                              condition_key, neighbors, predictScanvi)
 
 
 def write_adata_to_csv(model, adata=None, key=None, filename=tempfile.mktemp(), drop_columns=None,
-                       cell_type_key='cell_type',
+                       unlabeled_category='Unknown', cell_type_key='cell_type',
                        condition_key='study', neighbors=8, predictScanvi=False):
     anndata = None
     if adata is None:
@@ -54,6 +67,7 @@ def write_adata_to_csv(model, adata=None, key=None, filename=tempfile.mktemp(), 
     scanpy.tl.umap(latent)
     if predictScanvi:
         latent.obs['predictions'] = model.predict(adata=adata)
+
     return write_latent_csv(latent, key, filename)
 
 
