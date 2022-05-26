b44eb85369ec9e9c0df3d40fae9da2a8aed08e8c
Cem Nasit Sarica <cemnasitsarica@gmail.com>
2022-05-26 19:29:53 +0200

    Bugfix teampage using return before useffect

--- Frontend/src/components/teams/detail/TeamProjectList/index.jsx
+++ Frontend/src/components/teams/detail/TeamProjectList/index.jsx
@@ -25,13 +25,6 @@ function TeamProjectList({
   const [removedProject, setRemovedProject] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  if ((!team.memberIds?.includes(user._id) && team.visibility === 'PRIVATE')
-    || (team.visibility === 'BY_INSTITUTION' && !institution.memberIds?.includes(user._id))) {
-    return (
-      <span>{`The projects of this team are hidden as it's visibility is set to ${team.visibility.toLowerCase().replace('_', ' ')} and you're not a member of this team${team.visibility === 'BY_INSTITUTION' ? " or this team's institution" : ''}.`}</span>
-    );
-  }
-
   const handleCloseDialog = () => setDialogOpen(false);
 
   function handleDelete(project) {
@@ -69,6 +62,13 @@ function TeamProjectList({
     ProjectService.getTeamProjects(team._id).then((data) => setProjects(data));
     setIsLoading(false);
   }, []);
+
+  if ((!team.memberIds?.includes(user._id) && team.visibility === 'PRIVATE')
+    || (team.visibility === 'BY_INSTITUTION' && !institution.memberIds?.includes(user._id))) {
+    return (
+      <span>{`The projects of this team are hidden as it's visibility is set to ${team.visibility.toLowerCase().replace('_', ' ')} and you're not a member of this team${team.visibility === 'BY_INSTITUTION' ? " or this team's institution" : ''}.`}</span>
+    );
+  }
 
   if (isLoading) {
     return <CircularProgress />;
