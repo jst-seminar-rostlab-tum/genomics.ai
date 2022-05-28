178ea76f5c0b1e300f24b1a40b0d0fe9fb0e63b4
Paul Schwind <paul@pschwind.de>
2022-05-28 18:21:06 +0200

    tooltip for leave button when you are the only admin of the institution

--- Frontend/src/components/teams/overview/TeamLeaveButton/index.jsx
+++ Frontend/src/components/teams/overview/TeamLeaveButton/index.jsx
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Button from 'components/CustomButton';
 import { Modal, ModalTitle } from 'components/Modal';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
+import Tooltip from '@mui/material/Tooltip';
 
+import { useAuth } from 'shared/context/authContext';
 import TeamService from 'shared/services/Team.service';
 
 function TeamLeaveButton({ team, onLeft }) {
   const [dialogOpen, setDialogOpen] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [user] = useAuth();
 
   const handleOpenDialog = () => setDialogOpen(true);
   const handleCloseDialog = () => setDialogOpen(false);
@@ -25,14 +28,24 @@ function TeamLeaveButton({ team, onLeft }) {
     }
   }
 
+  const [onlyAdmin, setOnlyAdmin] = useState();
+  useEffect(() => {
+    setOnlyAdmin(team.adminIds.length === 1 && team.adminIds[0] === user._id);
+  }, [team, user]);
+
   return (
     <>
-      <Button
-        type="critical"
-        onClick={handleOpenDialog}
-      >
-        Leave
-      </Button>
+      <Tooltip title={onlyAdmin ? 'You are the only admin of this institution.' : ''}>
+        <div>
+          <Button
+            type="critical"
+            onClick={handleOpenDialog}
+            disabled={onlyAdmin}
+          >
+            Leave
+          </Button>
+        </div>
+      </Tooltip>
       <Modal
         isOpen={dialogOpen}
         setOpen={(o) => !o && handleCloseDialog()}
